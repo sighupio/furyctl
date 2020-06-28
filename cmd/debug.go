14273ecdaa0e606e19e7fe8f32401bfe9db1c3f5
Armando Ruocco <godhasu@gmail.com>
2020-06-28 22:03:22 +0200

    more comments

--- cmd/debug.go
+++ cmd/debug.go
@@ -16,6 +16,7 @@ const (
 	colorWhite  = "\033[37m"
 )
 // LogIfDebugOn prints a message on console if the flag --debug was passed to furyctl
+// can be changed to act as an abstraction layer over the concrete logging library
 func LogIfDebugOn(printf string, arguments ...interface{}) {
 	debug, err := rootCmd.Flags().GetBool("debug")
 
