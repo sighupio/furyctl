e94faaac26e40371f7e2d866722aa1b9d61dfd6f
Ángel Barrera Sánchez <angel@sighup.io>
2020-12-10 18:07:03 +0100

    Add more variables

--- data/provisioners/bootstrap/aws/output.tf
+++ data/provisioners/bootstrap/aws/output.tf
@@ -8,3 +8,38 @@ output "vpn_ip" {
   description = "VPN instance IP"
   value       = module.vpc-and-vpn.vpn_ip
 }
+
+output "vpn_operator_name" {
+  description = "SSH Username to log into the VPN Instance"
+  value       = var.vpn_operator_name
+}
+
+output "vpc_id" {
+  description = "The ID of the VPC"
+  value       = module.vpc-and-vpn.vpc_id
+}
+
+output "vpc_cidr_block" {
+  description = "The CIDR block of the VPC"
+  value       = module.vpc-and-vpn.vpc_cidr_block
+}
+
+output "public_subnets" {
+  description = "List of IDs of public subnets"
+  value       = module.vpc-and-vpn.public_subnets
+}
+
+output "public_subnets_cidr_blocks" {
+  description = "List of cidr_blocks of public subnets"
+  value       = module.vpc-and-vpn.public_subnets_cidr_blocks
+}
+
+output "private_subnets" {
+  description = "List of IDs of private subnets"
+  value       = module.vpc-and-vpn.private_subnets
+}
+
+output "private_subnets_cidr_blocks" {
+  description = "List of cidr_blocks of private subnets"
+  value       = module.vpc-and-vpn.private_subnets_cidr_blocks
+}
