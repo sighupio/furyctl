472f5f5fa94f54ec7782ca94f01b9a408436b8a5
Ángel Barrera Sánchez <angel@sighup.io>
2020-12-10 18:07:03 +0100

    Add more variables

--- data/provisioners/bootstrap/aws/main.tf
+++ data/provisioners/bootstrap/aws/main.tf
@@ -1,11 +1,17 @@
-
 module "vpc-and-vpn" {
-  source                   = "https://github.com/sighupio/furyctl-provisioners/archive/v0.1.0.zip//furyctl-provisioners-0.1.0/modules/bootstrap/aws/vpc-and-vpn"
+  # source = "https://github.com/sighupio/furyctl-provisioners/archive/v0.1.0.zip//furyctl-provisioners-0.1.0/modules/bootstrap/aws/vpc-and-vpn"
+  source = "https://github.com/sighupio/furyctl-provisioners/archive/main.zip//furyctl-provisioners-main/modules/bootstrap/aws/vpc-and-vpn"
+
   name                     = var.name
   network_cidr             = var.network_cidr
   public_subnetwork_cidrs  = var.public_subnetwork_cidrs
   private_subnetwork_cidrs = var.private_subnetwork_cidrs
   vpn_subnetwork_cidr      = var.vpn_subnetwork_cidr
+  vpn_port                 = var.vpn_port
+  vpn_instance_type        = var.vpn_instance_type
+  vpn_instance_disk_size   = var.vpn_instance_disk_size
+  vpn_operator_name        = var.vpn_operator_name
+  vpn_dhparams_bits        = var.vpn_dhparams_bits
   vpn_operator_cidrs       = var.vpn_operator_cidrs
   vpn_ssh_users            = var.vpn_ssh_users
 }
