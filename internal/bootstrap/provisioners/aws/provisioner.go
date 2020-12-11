3cd1d0cfb4810d797626f8e97b937538c0db160c
Ángel Barrera Sánchez <angel@sighup.io>
2020-12-11 12:17:10 +0100

    Improve the cli messages

--- internal/bootstrap/provisioners/aws/provisioner.go
+++ internal/bootstrap/provisioners/aws/provisioner.go
@@ -18,11 +18,13 @@ import (
 func (d *AWS) InitMessage() string {
 	return `[AWS] - VPC and VPN
 
-This provisioner creates a battle-tested AWS VPC with
-all the requirements set to run a production-grade private EKS cluster.
+This provisioner creates a battle-tested AWS VPC with all the requirements
+set to run a production-grade private EKS cluster.
 
-It creates a VPN server so you can deploy the cluster from this computer
+It creates a VPN server enables deploying the cluster from this computer
 once connected to the VPN server.
+
+Then, use furyagent to manage VPN profiles.
 `
 }
 
@@ -32,15 +34,27 @@ func (d *AWS) UpdateMessage() string {
 	output, _ = d.terraform.Output(context.Background())
 	spec := d.config.Spec.(cfg.AWS)
 	sshUsers := spec.VPN.SSHUsers
-	var vpnInstanceIP, vpnOperatorName string
+	var vpnInstanceIP, vpnOperatorName, vpcID string
+	var publicSubnetsIDs, privateSubnetsIDs []string
 	json.Unmarshal(output["vpn_ip"].Value, &vpnInstanceIP)
 	json.Unmarshal(output["vpn_operator_name"].Value, &vpnOperatorName)
+	json.Unmarshal(output["vpc_id"].Value, &vpcID)
+	json.Unmarshal(output["public_subnets"].Value, &publicSubnetsIDs)
+	json.Unmarshal(output["private_subnets"].Value, &privateSubnetsIDs)
+
 	return fmt.Sprintf(`[AWS] - VPC and VPN
 
 All the bootstrap components are up to date.
 
+VPC and VPN ready:
+
+VPC: %v
+Public Subnets: %v
+Private Subnets: %v
+
 Your VPN instance IP is: %v
-Use the ssh %v username to access the VPN instance with any SSH key configured for the following GitHub users: %v.
+Use the ssh %v username to access the VPN instance with any SSH key configured
+for the following GitHub users: %v.
 
 $ ssh %v@%v
 
@@ -52,16 +66,18 @@ Discover already registered vpn clients running:
 
 $ furyagent configure openvpn-client --list --config %v/furyagent.yml
 
-IMPORTANT! Connect to the VPN with the created ovpn profile to continue deploying an AWS Kubernetes cluster.
-`, vpnInstanceIP, vpnOperatorName, sshUsers, vpnOperatorName, vpnInstanceIP, d.terraform.WorkingDir(), d.terraform.WorkingDir())
+IMPORTANT! Connect to the VPN with the created ovpn profile to continue deploying
+an AWS Kubernetes cluster.
+`, vpcID, publicSubnetsIDs, privateSubnetsIDs, vpnInstanceIP, vpnOperatorName, sshUsers, vpnOperatorName, vpnInstanceIP, d.terraform.WorkingDir(), d.terraform.WorkingDir())
 }
 
 // DestroyMessage return a custom provisioner message the user will see once the cluster is destroyed
 func (d *AWS) DestroyMessage() string {
 	return `[AWS] - VPC and VPN
-All the bootstrap components has been destroyed. VPN and VPC gone away.
+All bootstrap components were destroyed.
+VPN and VPC went away.
 
-If you had any problem, contact us at sales@sighup.io.
+Had problems, contact us at sales@sighup.io.
 `
 }
 
