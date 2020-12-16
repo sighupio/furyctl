01bc9022b38255a0c721688aeaa075161c616057
Ángel Barrera Sánchez <angel@sighup.io>
2020-12-16 08:53:17 +0100

    Add license header to all files

--- data/provisioners/bootstrap/aws/variables.tf
+++ data/provisioners/bootstrap/aws/variables.tf
@@ -1,3 +1,9 @@
+/**
+ * Copyright (c) 2020 SIGHUP s.r.l All rights reserved.
+ * Use of this source code is governed by a BSD-style
+ * license that can be found in the LICENSE file.
+ */
+
 variable "name" {
   description = "Name of the resources. Used as cluster name"
   type        = string
