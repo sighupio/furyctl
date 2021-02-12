5dff59af201ed712bdf6e401bf32fd96c21c5687
Ángel Barrera Sánchez <angel@sighup.io>
2021-02-12 10:03:29 +0100

    Check project structures

--- automated-tests/integration/gcp-gke/tests.sh
+++ automated-tests/integration/gcp-gke/tests.sh
@@ -36,12 +36,52 @@ fi
     [ "$status" -eq 0 ]
 }
 
+@test "Bootstrap structure" {
+    info
+    project_dir="./automated-tests/integration/gcp-gke/bootstrap"
+    test(){
+        if [ -e ${project_dir}/bin/terraform ] && [ -e ${project_dir}/configuration/.netrc ] && [ -e ${project_dir}/logs/terraform.logs ] && [ -e ${project_dir}/.gitignore ] && [ -e ${project_dir}/.gitattributes ] && [ -e ${project_dir}/.gitattributes ]
+        then
+            echo "  All files exist, directory intact" >&3
+            return 0
+        else
+            echo "  One or more files are missing" >&3
+            return 1
+        fi
+    }
+    run test
+    if [[ $status -ne 0 ]]; then
+        echo "$output" >&3
+    fi
+    [ "$status" -eq 0 ]
+}
+
 @test "Cluster init" {
     info
     init(){
         ./dist/furyctl-${OS}_${OS}_amd64/furyctl -d --debug cluster init --config ./automated-tests/integration/gcp-gke/cluster.yml -w ./automated-tests/integration/gcp-gke/cluster --reset
     }
     run init
+    if [[ $status -ne 0 ]]; then
+        echo "$output" >&3
+    fi
+    [ "$status" -eq 0 ]
+}
+
+@test "Cluster structure" {
+    info
+    project_dir="./automated-tests/integration/gcp-gke/cluster"
+    test(){
+        if [ -e ${project_dir}/bin/terraform ] && [ -e ${project_dir}/configuration/.netrc ] && [ -e ${project_dir}/logs/terraform.logs ] && [ -e ${project_dir}/.gitignore ] && [ -e ${project_dir}/.gitattributes ] && [ -e ${project_dir}/backend.tf ]
+        then
+            echo "  All files exist, directory intact" >&3
+            return 0
+        else
+            echo "  One or more files are missing" >&3
+            return 1
+        fi
+    }
+    run test
     if [[ $status -ne 0 ]]; then
         echo "$output" >&3
     fi
