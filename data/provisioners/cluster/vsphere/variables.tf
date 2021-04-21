314641dc80559de61509b2709e74c19f270cacee
Ángel Barrera Sánchez <angel@sighup.io>
2021-04-21 15:44:21 +0200

    FIX: add missing license

--- data/provisioners/cluster/vsphere/variables.tf
+++ data/provisioners/cluster/vsphere/variables.tf
@@ -1,3 +1,9 @@
+/**
+ * Copyright (c) 2021 SIGHUP s.r.l All rights reserved.
+ * Use of this source code is governed by a BSD-style
+ * license that can be found in the LICENSE file.
+ */
+
 variable "name" {
   type        = string
   description = "Cluster name"
