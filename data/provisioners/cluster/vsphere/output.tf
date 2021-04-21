00da1b9083818a32c0bef9a460d367535212250d
Ángel Barrera Sánchez <angel@sighup.io>
2021-04-21 15:44:21 +0200

    FIX: add missing license

--- data/provisioners/cluster/vsphere/output.tf
+++ data/provisioners/cluster/vsphere/output.tf
@@ -1,3 +1,9 @@
+/**
+ * Copyright (c) 2021 SIGHUP s.r.l All rights reserved.
+ * Use of this source code is governed by a BSD-style
+ * license that can be found in the LICENSE file.
+ */
+
 output "ansible_inventory" {
   value       = module.fury.ansible_inventory
   description = "The ansible inventory to be used as hosts.ini file"
