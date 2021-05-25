1fb0f71a82eb11a096a024f3bfbf20a94d90468d
Ángel Barrera Sánchez <angel@sighup.io>
2021-05-25 11:09:48 +0200

    Trying to solve vsphere timeout error while destroying cluster

--- internal/cluster/provisioners/vsphere/provisioner.go
+++ internal/cluster/provisioners/vsphere/provisioner.go
@@ -338,7 +338,7 @@ func downloadAnsibleRoles(workingDirectory string) error {
 	}
 
 	client := &getter.Client{
-		Src:  "https://github.com/sighupio/furyctl-provisioners/archive/v0.5.0-rc0.zip//furyctl-provisioners-0.5.0-rc0/roles",
+		Src:  "https://github.com/sighupio/furyctl-provisioners/archive/v0.5.0-rc1.zip//furyctl-provisioners-0.5.0-rc1/roles",
 		Dst:  downloadPath,
 		Pwd:  workingDirectory,
 		Mode: getter.ClientModeAny,
