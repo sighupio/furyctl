b3c466edfcabf5a8cbd39c22f1a0cd3abe86a0fb
Ángel Barrera Sánchez <angel@sighup.io>
2021-07-05 17:24:15 +0200

    Adding 1.7.1 eks provisioner

--- data/provisioners/cluster/eks/main.tf
+++ data/provisioners/cluster/eks/main.tf
@@ -17,7 +17,7 @@ terraform {
 }
 
 module "fury" {
-  source = "github.com/sighupio/fury-eks-installer//modules/eks?ref=v1.7.1-rc0"
+  source = "github.com/sighupio/fury-eks-installer//modules/eks?ref=v1.7.1"
 
   cluster_name    = var.cluster_name
   cluster_version = var.cluster_version
