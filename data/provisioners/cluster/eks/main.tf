e3aba9c155dc9d927adb26fa23cc3df667e92d5f
Ángel Barrera Sánchez <angel@sighup.io>
2021-09-06 12:33:17 +0200

    Using stable version of the cloud-installers

--- data/provisioners/cluster/eks/main.tf
+++ data/provisioners/cluster/eks/main.tf
@@ -18,7 +18,7 @@ terraform {
 }
 
 module "fury" {
-  source = "github.com/sighupio/fury-eks-installer//modules/eks?ref=no-spot-instance_eks-ami-id-rename"
+  source = "github.com/sighupio/fury-eks-installer//modules/eks?ref=v1.8.0"
 
   cluster_name    = var.cluster_name
   cluster_version = var.cluster_version
