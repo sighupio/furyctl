dca78780d6918ae1abd1449c26120f1c36ee8237
Samuele Chiocca <samuele@sighup.io>
2022-03-31 11:41:05 +0200

    WIP gke works again

--- data/provisioners/cluster/gke/main.tf
+++ data/provisioners/cluster/gke/main.tf
@@ -18,7 +18,7 @@ terraform {
 }
 
 module "fury" {
-  source = "github.com/sighupio/fury-gke-installer//modules/gke?ref=v1.8.1"
+  source = "github.com/sighupio/fury-gke-installer//modules/gke?ref=v1.9.0-rc2"
 
   cluster_name    = var.cluster_name
   cluster_version = var.cluster_version
