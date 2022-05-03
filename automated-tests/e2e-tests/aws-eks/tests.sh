d08f8bd64f984c6e5b5ebccbd17e4a7bfacec2b7
Samuele Chiocca <samuele@sighup.io>
2022-05-03 10:49:14 +0200

    Port spotInstance feature from furyctl-next branch, and all the minor changes on the copyright side

--- automated-tests/e2e-tests/aws-eks/tests.sh
+++ automated-tests/e2e-tests/aws-eks/tests.sh
@@ -197,15 +197,45 @@ fi
     [ "$status" -eq 0 ]
 }
 
-@test "kubectl" {
+@test "kubectl get pods" {
     info
     cluster_info(){
         export KUBECONFIG=./automated-tests/e2e-tests/aws-eks/cluster/secrets/kubeconfig
         kubectl get pods -A >&3
-        kubectl get nodes -o wide >&3
     }
     run cluster_info
 
+    if [[ $status -ne 0 ]]; then
+        echo "$output" >&3
+        cat ./automated-tests/e2e-tests/aws-eks/cluster/secrets/kubeconfig >&3
+    fi
+    [ "$status" -eq 0 ]
+}
+
+@test "kubectl get nodes" {
+    info
+    cluster_info(){
+        export KUBECONFIG=./automated-tests/e2e-tests/aws-eks/cluster/secrets/kubeconfig
+        kubectl get nodes -o wide --show-labels >&3
+    }
+    run cluster_info
+
+    if [[ $status -ne 0 ]]; then
+        echo "$output" >&3
+        cat ./automated-tests/e2e-tests/aws-eks/cluster/secrets/kubeconfig >&3
+    fi
+    [ "$status" -eq 0 ]
+}
+
+@test "kubectl get nodes verify spot presence" {
+    info
+    test(){
+        export KUBECONFIG=./automated-tests/e2e-tests/aws-eks/cluster/secrets/kubeconfig
+        data=$(kubectl get nodes --show-labels | grep "node.kubernetes.io/lifecycle=spot")
+        if [ "${data}" == "" ]; then return 1; fi
+    }
+    loop_it test 60 5
+    status=${loop_it_result}
     if [[ $status -ne 0 ]]; then
         echo "$output" >&3
         cat ./automated-tests/e2e-tests/aws-eks/cluster/secrets/kubeconfig >&3
--- go.mod
+++ go.mod
@@ -6,22 +6,19 @@ require (
 	github.com/denisbrodbeck/machineid v1.0.1
 	github.com/dukex/mixpanel v0.0.0-20180925151559-f8d5594f958e
 	github.com/fatih/color v1.10.0 // indirect
-	github.com/gobuffalo/logger v1.0.4 // indirect
-	github.com/gobuffalo/packr/v2 v2.8.1
+	github.com/gobuffalo/packr/v2 v2.8.3
 	github.com/hashicorp/go-checkpoint v0.5.0
 	github.com/hashicorp/go-getter v1.5.3
 	github.com/hashicorp/go-version v1.3.0
 	github.com/hashicorp/terraform-exec v0.13.3
-	github.com/karrick/godirwalk v1.16.1 // indirect
 	github.com/relex/aini v1.2.1
-	github.com/rogpeppe/go-internal v1.8.0 // indirect
+	github.com/rogpeppe/go-internal v1.8.1 // indirect
 	github.com/sirupsen/logrus v1.8.1
-	github.com/spf13/cobra v1.2.1
+	github.com/spf13/cobra v1.4.0
 	github.com/spf13/viper v1.8.1
-	golang.org/x/crypto v0.0.0-20210513164829-c07d793c2f9a // indirect
-	golang.org/x/sys v0.0.0-20210917161153-d61c044b1678 // indirect
-	golang.org/x/term v0.0.0-20210916214954-140adaaadfaf // indirect
-	golang.org/x/tools v0.1.6 // indirect
+	golang.org/x/sys v0.0.0-20220502124256-b6088ccd6cba // indirect
+	golang.org/x/term v0.0.0-20220411215600-e5f449aeb171 // indirect
+	golang.org/x/tools v0.1.10 // indirect
 	gopkg.in/yaml.v2 v2.4.0
 )
 
