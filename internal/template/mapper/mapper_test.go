f52a504dbbcfc6b80920e1cf2f05901835c727b4
Alessio Pragliola <alessio.pragliola@sighup.io>
2022-07-05 19:32:06 +0200

    chore: added tests to yaml, mapper

---
+++ internal/template/mapper/mapper_test.go
@@ -0,0 +1,98 @@
+// Copyright (c) 2022 SIGHUP s.r.l All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package mapper_test
+
+import (
+	"fmt"
+	"github.com/sighupio/furyctl/internal/template/mapper"
+	"github.com/stretchr/testify/assert"
+	"os"
+	"testing"
+)
+
+func TestNewMapper(t *testing.T) {
+	dummyContext := map[string]map[any]any{
+		"data": {
+			"meta": map[any]any{
+				"name": "test",
+			},
+		},
+	}
+
+	m := mapper.NewMapper(dummyContext)
+
+	assert.NotNil(t, m)
+}
+
+func TestMapper_MapEnvironmentVars(t *testing.T) {
+	dummyContext := map[string]map[any]any{
+		"data": {
+			"meta": map[any]any{
+				"name": "test",
+			},
+		},
+	}
+
+	expectedEnvMap := map[string]string{
+		"TEST_MAPPER_ENV": "test",
+	}
+
+	m := mapper.NewMapper(dummyContext)
+
+	err := os.Setenv("TEST_MAPPER_ENV", "test")
+
+	assert.NoError(t, err)
+
+	defer os.Setenv("TEST_MAPPER_ENV", "")
+
+	envMap := m.MapEnvironmentVars()
+
+	assert.Equal(t, expectedEnvMap["TEST_MAPPER_ENV"], envMap["TEST_MAPPER_ENV"])
+}
+
+func TestMapper_MapDynamicValues(t *testing.T) {
+	path, err := os.MkdirTemp("", "test")
+
+	assert.NoError(t, err)
+
+	exampleStr := "test!"
+
+	err = os.WriteFile(path+"/test_file.txt", []byte(exampleStr), os.ModePerm)
+
+	defer os.RemoveAll(path)
+
+	assert.NoError(t, err)
+
+	dummyContext := map[string]map[any]any{
+		"data": {
+			"meta": map[any]any{
+				"name":  map[any]any{"env://TEST_MAPPER_DYNAMIC_VALUE": ""},
+				"value": map[any]any{fmt.Sprintf("file://%s/test_file.txt", path): ""},
+			},
+		},
+	}
+
+	m := mapper.NewMapper(dummyContext)
+
+	err = os.Setenv("TEST_MAPPER_DYNAMIC_VALUE", "test")
+
+	assert.NoError(t, err)
+
+	defer os.Setenv("TEST_MAPPER_DYNAMIC_VALUE", "")
+
+	filledContext, err := m.MapDynamicValues()
+
+	assert.NoError(t, err)
+
+	meta := filledContext["data"]["meta"]
+
+	mapMeta, ok := meta.(map[any]any)
+
+	assert.True(t, ok)
+
+	assert.Equal(t, "test", mapMeta["name"])
+
+	assert.Equal(t, exampleStr, mapMeta["value"])
+}
---
+++ internal/yaml/yaml_test.go
@@ -0,0 +1,47 @@
+// Copyright (c) 2022 SIGHUP s.r.l All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package yaml_test
+
+import (
+	yaml2 "github.com/sighupio/furyctl/internal/yaml"
+	"github.com/stretchr/testify/assert"
+	"gopkg.in/yaml.v3"
+	"os"
+	"testing"
+)
+
+type TestYaml struct {
+	Test string `yaml:"test"`
+}
+
+func TestFromFile(t *testing.T) {
+	test := TestYaml{
+		"test",
+	}
+
+	path, err := os.MkdirTemp("", "test")
+
+	assert.NoError(t, err)
+
+	file, err := os.Create(path + "/test.yaml")
+
+	assert.NoError(t, err)
+
+	testBytes, err := yaml.Marshal(test)
+
+	assert.NoError(t, err)
+
+	_, err = file.Write(testBytes)
+
+	assert.NoError(t, err)
+
+	defer os.RemoveAll(path)
+
+	testRes, err := yaml2.FromFile[TestYaml](path + "/test.yaml")
+
+	assert.NoError(t, err)
+
+	assert.Equal(t, test, testRes)
+}
