76abea1d53460db6ea7318890e7d161a45c6c538
Alessio Pragliola <alessio.pragliola@sighup.io>
2022-07-28 20:15:01 +0200

    feat: added integration tests

---
+++ automated-tests/integration/template-engine/tests.sh
@@ -0,0 +1,56 @@
+#!/usr/bin/env bats
+# Copyright (c) 2017-present SIGHUP s.r.l All rights reserved.
+# Use of this source code is governed by a BSD-style
+# license that can be found in the LICENSE file.
+
+
+load "./../../helper"
+
+OS="linux"
+if [[ "${OSTYPE}" == "darwin"* ]]; then
+    OS="darwin"
+fi
+CPUARCH="amd64_v1"
+if [ "$(uname -m)" = "arm64" ]; then
+	CPUARCH="arm64"
+fi
+
+@test "furyctl" {
+    info
+    init(){
+        ./dist/furyctl-${OS}_${OS}_${CPUARCH}/furyctl version
+    }
+    run init
+    if [[ ${status} -ne 0 ]]; then
+        echo "${output}" >&3
+    fi
+    [ "${status}" -eq 0 ]
+}
+
+@test "template simple-dry-run" {
+    info
+    init(){
+        cd ./test-data/simple-dry-run && ../../dist/furyctl-${OS}_${OS}_${CPUARCH}/furyctl -d --debug template --dry-run
+        cat ./target/file.txt | grep "testValue"
+    }
+    run init
+
+    if [[ ${status} -ne 0 ]]; then
+        echo "${output}" >&3
+    fi
+    [ "${status}" -eq 0 ]
+}
+
+@test "template simple" {
+    info
+    init(){
+        cd ./test-data/simple && ../../dist/furyctl-${OS}_${OS}_${CPUARCH}/furyctl -d --debug template
+        cat ./target/file.txt | grep "testValue"
+    }
+    run init
+
+    if [[ ${status} -ne 0 ]]; then
+        echo "${output}" >&3
+    fi
+    [ "${status}" -eq 0 ]
+}
