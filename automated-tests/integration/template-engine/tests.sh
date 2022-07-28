7ed118b98542b987cb62ec8ab076477e97f77b86
Alessio Pragliola <alessio.pragliola@sighup.io>
2022-07-28 21:24:35 +0200

    fix: integration test pt2

--- automated-tests/integration/template-engine/tests.sh
+++ automated-tests/integration/template-engine/tests.sh
@@ -31,7 +31,7 @@ fi
     info
     test_dir="./automated-tests/integration/template-engine/test-data/simple-dry-run"
     init(){
-        cd ${test_dir} && ../../dist/furyctl-${OS}_${OS}_${CPUARCH}/furyctl -d --debug template --dry-run
+        cd ${test_dir} && ../../../../../dist/furyctl-${OS}_${OS}_${CPUARCH}/furyctl -d --debug template --dry-run
         cat ./target/file.txt | grep "testValue"
     }
     run init
@@ -46,7 +46,7 @@ fi
     info
     test_dir="./automated-tests/integration/template-engine/test-data/simple"
     init(){
-        cd ${test_dir} && ../../dist/furyctl-${OS}_${OS}_${CPUARCH}/furyctl -d --debug template
+        cd ${test_dir} && ../../../../../dist/furyctl-${OS}_${OS}_${CPUARCH}/furyctl -d --debug template
         cat ./target/file.txt | grep "testValue"
     }
     run init
