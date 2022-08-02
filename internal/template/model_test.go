40f4f70b6e911a5097d5c290763005989196cdfc
Alessio Pragliola <83355398+Al-Pragliola@users.noreply.github.com>
2022-08-02 12:08:36 +0200

    Feature/template engine (#81)
    * feat: initial template engine feature
    
    * feat: initial merge feature
    
    * feat: fixed env/file replacement
    
    * feat: refactor merge
    
    * chore: wip walk logic
    
    * chore: fixed walk function
    
    * chore: merge tests and small refactoring
    
    * chore: refactoring of template engine
    
    * chore: upped go version; further refactoring
    
    * chore: refactoring
    
    * chore: template model and mapper refactoring
    
    * fix: skipping files with only whitespaces
    
    * feat: added --dry-run to template cmd [WIP]
    
    * chore: refactoring applyTemplates func
    
    * fix: template tests + added test on funcmap
    
    * fix: added licenses
    
    * chore: added tests to yaml, mapper
    
    * feat: added tests / refactoring on node_fields
    
    * fix: licenses
    
    * chore: moved function up
    
    * chore: refactor map to alias
    
    * chore: removed empty line
    
    * chore: refactoring template model
    
    * fix: broken test
    
    * fix: post review
    
    * chore: removed default from template suffix
    
    * fix: typo in last commit
    
    * feat: added filename in yaml parsing errors in template cmd
    
    * chore: work on change requests pt.1
    
    Co-authored-by: Claudio Beatrice <claudi0.beatric3@gmail.com>
    
    * chore: work on change requests - model_test
    
    Co-authored-by: Claudio Beatrice <claudi0.beatric3@gmail.com>
    
    * chore: work on change requests pt.2
    
    Co-authored-by: Claudio Beatrice <claudi0.beatric3@gmail.com>
    
    * chore: added task to todo
    
    * fix: bugs after reverting to yamlv.2
    
    * feat: added integration tests
    
    * fix: added licenses
    
    * fix: integration test
    
    * fix: integration test pt2
    
    * fix: integration test pt3
    
    * Update cmd/template.go
    
    Co-authored-by: Ramiro Algozino <ramiro@sighup.io>
    
    * fix: linting
    
    * fix: removed cmd banner on error
    
    * fix: removed double error
    
    * chore: missing NLs
    
    Co-authored-by: Claudio Beatrice <claudi0.beatric3@gmail.com>
    
    * chore: add more furyctl template tests, fix some loggin and descriptions
    
    * fix: linting
    
    Co-authored-by: Luigi Barbato <luigi.barbato@sighup.io>
    Co-authored-by: Claudio Beatrice <claudi0.beatric3@gmail.com>
    Co-authored-by: Ramiro Algozino <ramiro@sighup.io>

---
+++ internal/template/model_test.go
@@ -0,0 +1,52 @@
+// Copyright (c) 2017-present SIGHUP s.r.l All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package template_test
+
+import (
+	"github.com/sighupio/furyctl/internal/template"
+	"github.com/stretchr/testify/assert"
+	"gopkg.in/yaml.v2"
+	"os"
+	"testing"
+)
+
+func TestNewTemplateModel(t *testing.T) {
+	conf := map[string]interface{}{
+		"data": map[string]interface{}{
+			"meta": map[string]string{
+				"name": "test",
+			},
+		},
+	}
+
+	templateTest := "A nice day at {{.meta.name | substr 0 3}}"
+
+	confYaml, err := yaml.Marshal(conf)
+	if err != nil {
+		panic(err)
+	}
+
+	path, err := os.MkdirTemp("", "test")
+
+	err = os.Mkdir(path+"/source", os.ModePerm)
+	err = os.Mkdir(path+"/target", os.ModePerm)
+	err = os.WriteFile(path+"/source/test.md.tpl", []byte(templateTest), os.ModePerm)
+	err = os.WriteFile(path+"/configTest.yaml", confYaml, os.ModePerm)
+
+	defer os.RemoveAll(path)
+
+	tm, err := template.NewTemplateModel(
+		path+"/source",
+		path+"/target",
+		path+"/configTest.yaml",
+		path,
+		".tpl",
+		false,
+		false,
+	)
+
+	assert.NoError(t, err)
+	assert.NotNil(t, tm)
+}
