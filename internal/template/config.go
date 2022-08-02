c65aea2c0beb8f038df35a2132bbe36c3a5ab7f4
Alessio Pragliola <83355398+Al-Pragliola@users.noreply.github.com>
2022-08-02 12:08:36 +0200

    Feature/template engine (#81)
    * feat: initial template engine feature
    
    * feat: initial merge feature
    
    * feat: fixed env/file replacement
    
    * feat: refactor merge
    
    * chore: wip walk logic
    
    * chore: fixed walk function
    
    * chore: merge tests and small refactoring
    
    * chore: refactoring of template engine
    
    * chore: upped go version; further refactoring
    
    * chore: refactoring
    
    * chore: template model and mapper refactoring
    
    * fix: skipping files with only whitespaces
    
    * feat: added --dry-run to template cmd [WIP]
    
    * chore: refactoring applyTemplates func
    
    * fix: template tests + added test on funcmap
    
    * fix: added licenses
    
    * chore: added tests to yaml, mapper
    
    * feat: added tests / refactoring on node_fields
    
    * fix: licenses
    
    * chore: moved function up
    
    * chore: refactor map to alias
    
    * chore: removed empty line
    
    * chore: refactoring template model
    
    * fix: broken test
    
    * fix: post review
    
    * chore: removed default from template suffix
    
    * fix: typo in last commit
    
    * feat: added filename in yaml parsing errors in template cmd
    
    * chore: work on change requests pt.1
    
    Co-authored-by: Claudio Beatrice <claudi0.beatric3@gmail.com>
    
    * chore: work on change requests - model_test
    
    Co-authored-by: Claudio Beatrice <claudi0.beatric3@gmail.com>
    
    * chore: work on change requests pt.2
    
    Co-authored-by: Claudio Beatrice <claudi0.beatric3@gmail.com>
    
    * chore: added task to todo
    
    * fix: bugs after reverting to yamlv.2
    
    * feat: added integration tests
    
    * fix: added licenses
    
    * fix: integration test
    
    * fix: integration test pt2
    
    * fix: integration test pt3
    
    * Update cmd/template.go
    
    Co-authored-by: Ramiro Algozino <ramiro@sighup.io>
    
    * fix: linting
    
    * fix: removed cmd banner on error
    
    * fix: removed double error
    
    * chore: missing NLs
    
    Co-authored-by: Claudio Beatrice <claudi0.beatric3@gmail.com>
    
    * chore: add more furyctl template tests, fix some loggin and descriptions
    
    * fix: linting
    
    Co-authored-by: Luigi Barbato <luigi.barbato@sighup.io>
    Co-authored-by: Claudio Beatrice <claudi0.beatric3@gmail.com>
    Co-authored-by: Ramiro Algozino <ramiro@sighup.io>

---
+++ internal/template/config.go
@@ -0,0 +1,18 @@
+// Copyright (c) 2017-present SIGHUP s.r.l All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package template
+
+type Templates struct {
+	Includes        []string `yaml:"includes,omitempty"`
+	Excludes        []string `yaml:"excludes,omitempty"`
+	Suffix          string   `yaml:"suffix,omitempty"`
+	ProcessFilename bool     `yaml:"processFilename,omitempty"`
+}
+
+type Config struct {
+	Data      map[string]map[any]any `yaml:"data,omitempty"`
+	Include   map[string]string      `yaml:"include,omitempty"`
+	Templates Templates              `yaml:"templates,omitempty"`
+}
---
+++ internal/template/generator.go
@@ -0,0 +1,150 @@
+// Copyright (c) 2017-present SIGHUP s.r.l All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package template
+
+import (
+	"bytes"
+	"fmt"
+	"path/filepath"
+	"strings"
+	"text/template"
+
+	"github.com/sighupio/furyctl/internal/io"
+	"github.com/sirupsen/logrus"
+)
+
+type generator struct {
+	source  string
+	target  string
+	context map[string]map[any]any
+	funcMap FuncMap
+	dryRun  bool
+}
+
+func NewGenerator(
+	source,
+	target string,
+	context map[string]map[any]any,
+	funcMap FuncMap,
+	dryRun bool,
+) *generator {
+	return &generator{
+		source:  source,
+		target:  target,
+		context: context,
+		funcMap: funcMap,
+		dryRun:  dryRun,
+	}
+}
+
+func (g *generator) ProcessTemplate() *template.Template {
+	return template.Must(
+		template.New(filepath.Base(g.source)).Funcs(g.funcMap.FuncMap).ParseFiles(g.source))
+}
+
+func (g *generator) GetMissingKeys(tpl *template.Template) []string {
+	var missingKeys []string
+
+	node := NewNode()
+	node.FromNodeList(tpl.Tree.Root.Nodes)
+
+	for _, f := range node.Fields {
+		val := g.getContextValueFromPath(f)
+		if val == nil {
+			missingKeys = append(missingKeys, f)
+		}
+	}
+
+	return missingKeys
+}
+
+func (g *generator) ProcessFile(tpl *template.Template) (bytes.Buffer, error) {
+	var generatedContent bytes.Buffer
+
+	if !g.dryRun {
+		tpl.Option("missingkey=error")
+	}
+
+	err := tpl.Execute(&generatedContent, g.context)
+
+	return generatedContent, err
+}
+
+func (g *generator) ProcessFilename(
+	tm *Model,
+) (string, error) {
+	var realTarget string
+
+	if tm.Config.Templates.ProcessFilename { //try to process filename as template
+		tpl := template.Must(
+			template.New("currentTarget").Funcs(g.funcMap.FuncMap).Parse(g.target))
+
+		destination := bytes.NewBufferString("")
+
+		if err := tpl.Execute(destination, g.context); err != nil {
+			return "", err
+		}
+		realTarget = destination.String()
+	} else {
+		realTarget = g.target
+	}
+
+	suf := tm.Suffix
+	if strings.HasSuffix(realTarget, suf) {
+		realTarget = realTarget[:len(realTarget)-len(tm.Suffix)] //cut off extension (.tmpl) from the end
+	}
+
+	return realTarget, nil
+}
+
+func (g *generator) UpdateTarget(newTarget string) {
+	g.target = newTarget
+}
+
+func (g *generator) WriteMissingKeysToFile(
+	missingKeys []string,
+	tmplPath,
+	outputPath string,
+) error {
+	if len(missingKeys) == 0 {
+		return nil
+	}
+
+	logrus.Warnf(
+		"missing keys in template %s. Writing to %s/tmpl-debug.log\n",
+		tmplPath,
+		outputPath,
+	)
+
+	debugFilePath := filepath.Join(outputPath, "tmpl-debug.log")
+
+	outLog := fmt.Sprintf("[%s]\n%s\n", tmplPath, strings.Join(missingKeys, "\n"))
+
+	return io.AppendBufferToFile(*bytes.NewBufferString(outLog), debugFilePath)
+}
+
+func (g *generator) getContextValueFromPath(path string) any {
+	paths := strings.Split(path[1:], ".")
+
+	if len(paths) == 0 {
+		return nil
+	}
+
+	ret := g.context[paths[0]]
+
+	for _, key := range paths[1:] {
+		mapAtKey, ok := ret[key]
+		if !ok {
+			return nil
+		}
+
+		ret, ok = mapAtKey.(map[any]any)
+		if !ok {
+			return mapAtKey
+		}
+	}
+
+	return ret
+}
