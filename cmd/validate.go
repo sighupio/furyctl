4e889185c135033fab317ebde8ef2a150cc397c0
omissis <claudi0.beatric3@gmail.com>
2022-09-13 16:31:55 +0200

    feat: wip validate

--- cmd/validate.go
+++ cmd/validate.go
@@ -5,41 +5,54 @@ import (
 
 	"github.com/spf13/cobra"
 
+	"github.com/sighupio/furyctl/cmd/cmdutil"
 	"github.com/sighupio/furyctl/cmd/validate"
 	"github.com/sighupio/furyctl/internal/schema/santhosh"
 )
 
-var configPath string
-var output string
+var ErrSchemaDownload = fmt.Errorf("error downloading json schema for furyctl.yaml")
 
 var validateCmd = &cobra.Command{
 	Use:   "validate",
 	Short: "Validate Furyfile",
 	RunE: func(cmd *cobra.Command, args []string) error {
-		schemaLocation := "https://raw.githubusercontent.com/sighupio/fury-distribution/feature/create-draft-of-the-furyctl-yaml-json-schema"
+		distroLocation := cmd.Flag("distro-location").Value.String()
 
-		schema, err := santhosh.LoadSchema(schemaLocation)
+		schemasPath, err := validate.DownloadSchemas(distroLocation)
+		if err != nil {
+			return fmt.Errorf("%s: %w", ErrSchemaDownload, err)
+		}
+
+		hasErrors := error(nil)
+		furyctlFile, err := validate.ParseArgs(args)
+		if err != nil {
+			return err
+		}
+
+		minimalConf := cmdutil.LoadConfig[validate.FuryctlConfig](furyctlFile)
+
+		schemaPath := validate.GetSchemaPath(schemasPath, minimalConf)
+
+		schema, err := santhosh.LoadSchema(schemaPath)
 		if err != nil {
 			return fmt.Errorf("failed to load schema: %w", err)
 		}
 
-		hasErrors := error(nil)
+		conf := cmdutil.LoadConfig[any](furyctlFile)
 
-		conf := validate.LoadConfig[validate.FuryDistributionSpecs](configPath)
 		if err := schema.ValidateInterface(conf); err != nil {
-			validate.PrintResults(output, err, conf, configPath)
+			validate.PrintResults(err, conf, furyctlFile)
 
 			hasErrors = validate.ErrHasValidationErrors
 		}
 
-		validate.PrintSummary(output, hasErrors != nil)
+		validate.PrintSummary(hasErrors != nil)
 
 		return hasErrors
 	},
 }
 
 func init() {
-	validateCmd.Flags().StringP("config", "c", configPath, "Furyctl config file path")
-	validateCmd.Flags().StringP("output", "o", "text", "Output format (text|json)")
+	validateCmd.Flags().StringP("distro-location", "l", "", "Base URL used to download schemas.")
 	rootCmd.AddCommand(validateCmd)
 }
