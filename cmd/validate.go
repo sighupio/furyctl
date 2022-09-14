ed01fc0a6d9a8db1ca4f492be48cba6adac4ac11
omissis <claudi0.beatric3@gmail.com>
2022-09-14 19:33:38 +0200

    feat: refactor validate command and add versions validation
    - refactor yaml package to handle v2 and v3
    - improve error handling in root command
    - refactor "validate" command to "validate config" command
    - handle version compantibility in "validate config" command
    - refactor common "validate config" logic to make it available for the "validate dependencies" command

--- cmd/validate.go
+++ cmd/validate.go
@@ -1,104 +1,18 @@
 package cmd
 
 import (
-	"fmt"
-	"path/filepath"
-
 	"github.com/spf13/cobra"
 
-	"github.com/sighupio/furyctl/cmd/cmdutil"
 	"github.com/sighupio/furyctl/cmd/validate"
-	"github.com/sighupio/furyctl/internal/schema/santhosh"
 )
 
 var (
-	errDefaultsDownload = fmt.Errorf("error downloading json schema for furyctl.yaml")
-
 	validateCmd = &cobra.Command{
 		Use:   "validate",
-		Short: "Validate Furyfile",
-		RunE: func(cmd *cobra.Command, _ []string) error {
-			debug := cmd.Flag("debug").Value.String() == "true"
-			furyctlFilePath := cmd.Flag("config").Value.String()
-			schemasLocation := cmd.Flag("schemas-location").Value.String()
-			defaultsLocation := cmd.Flag("defaults-location").Value.String()
-
-			schemasPath, err := validate.DownloadFolder(schemasLocation, "schemas")
-			if err != nil {
-				return err
-			}
-			if !debug {
-				defer validate.CleanupTempDir(filepath.Base(schemasPath))
-			}
-
-			defaultsPath, err := validate.DownloadFolder(defaultsLocation, "defaults")
-			if err != nil {
-				return err
-			}
-			if !debug {
-				defer validate.CleanupTempDir(filepath.Base(defaultsPath))
-			}
-
-			hasErrors := error(nil)
-
-			minimalConf := cmdutil.LoadConfig[validate.FuryctlConfig](furyctlFilePath)
-
-			schemaPath := validate.GetSchemaPath(schemasPath, minimalConf)
-			defaultPath := validate.GetDefaultPath(defaultsPath, minimalConf)
-
-			defaultedFuryctlFilePath, err := validate.MergeConfigAndDefaults(furyctlFilePath, defaultPath)
-			if err != nil {
-				return err
-			}
-			if !debug {
-				defer validate.CleanupTempDir(filepath.Base(defaultedFuryctlFilePath))
-			}
-
-			schema, err := santhosh.LoadSchema(schemaPath)
-			if err != nil {
-				return err
-			}
-
-			conf := cmdutil.LoadConfig[any](defaultedFuryctlFilePath)
-
-			if err := schema.ValidateInterface(conf); err != nil {
-				validate.PrintResults(err, defaultedFuryctlFilePath)
-
-				hasErrors = validate.ErrHasValidationErrors
-			}
-
-			validate.PrintSummary(hasErrors != nil)
-
-			return hasErrors
-		},
+		Short: "Validate fury config files and dependencies",
 	}
 )
 
 func init() {
-	validateCmd.Flags().StringP(
-		"config",
-		"c",
-		"furyctl.yaml",
-		"Path to the furyctl.yaml file",
-	)
-
-	validateCmd.Flags().StringP(
-		"schemas-location",
-		"",
-		"",
-		"Base URL used to download schemas. "+
-			"It can either be a local path(eg: /path/to/fury/distribution//schemas) or "+
-			"a remote URL(eg: https://git@github.com/sighupio/fury-distribution//schemas?ref=BRANCH_NAME)."+
-			"Any format supported by hashicorp/go-getter can be used.",
-	)
-
-	validateCmd.Flags().StringP(
-		"defaults-location",
-		"",
-		"",
-		"Base URL used to download defaults. "+
-			"It can either be a local path(eg: /path/to/fury/distribution//defaults) or "+
-			"a remote URL(eg: https://git@github.com/sighupio/fury-distribution//defaults?ref=BRANCH_NAME)."+
-			"Any format supported by hashicorp/go-getter can be used.",
-	)
+	validateCmd.AddCommand(validate.NewConfigCmd(version))
 }
---
+++ cmd/validate/config.go
@@ -0,0 +1,186 @@
+package validate
+
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+
+	"github.com/sirupsen/logrus"
+	"github.com/spf13/cobra"
+
+	"github.com/sighupio/furyctl/internal/distribution"
+	"github.com/sighupio/furyctl/internal/merge"
+	"github.com/sighupio/furyctl/internal/schema/santhosh"
+	"github.com/sighupio/furyctl/internal/semver"
+	"github.com/sighupio/furyctl/internal/yaml"
+)
+
+const defaultBaseUrl = "https://git@github.com/sighupio/fury-distribution?ref=%s"
+
+var errHasValidationErrors = fmt.Errorf("furyctl.yaml contains validation errors")
+
+func NewConfigCmd(version string) *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "config",
+		Short: "Validate furyctl.yaml file",
+		RunE: func(cmd *cobra.Command, _ []string) error {
+			debug := cmd.Flag("debug").Value.String() == "true"
+			furyctlPath := cmd.Flag("config").Value.String()
+			distroLocation := cmd.Flag("distro-location").Value.String()
+
+			minimalConf, err := yaml.FromFileV3[distribution.FuryctlConfig](furyctlPath)
+			if err != nil {
+				return err
+			}
+
+			furyctlConfVersion := minimalConf.Spec.DistributionVersion
+
+			if version != "dev" {
+				furyctlBinVersion, err := semver.NewVersion(version)
+				if err != nil {
+					return err
+				}
+
+				sameMinors := semver.SameMinor(furyctlConfVersion, furyctlBinVersion)
+
+				if !sameMinors {
+					logrus.Warnf(
+						"this version of furyctl ('%s') does not support distribution version '%s', results may be inaccurate",
+						furyctlBinVersion,
+						furyctlConfVersion,
+					)
+				}
+			}
+
+			if distroLocation == "" {
+				distroLocation = fmt.Sprintf(defaultBaseUrl, furyctlConfVersion.String())
+			}
+
+			repoPath, err := downloadDirectory(distroLocation)
+			if err != nil {
+				return err
+			}
+			if !debug {
+				defer cleanupTempDir(filepath.Base(repoPath))
+			}
+
+			kfdPath := filepath.Join(repoPath, "kfd.yaml")
+			kfdManifest, err := yaml.FromFileV3[distribution.Manifest](kfdPath)
+			if err != nil {
+				return err
+			}
+
+			if !semver.SameMinor(furyctlConfVersion, kfdManifest.Version) {
+				return fmt.Errorf(
+					"minor versions mismatch: furyctl.yaml has %s, but furyctl has %s",
+					furyctlConfVersion.String(),
+					kfdManifest.Version.String(),
+				)
+			}
+
+			schemaPath := getSchemaPath(repoPath, minimalConf)
+			defaultPath := getDefaultPath(repoPath)
+
+			defaultedFuryctlPath, err := mergeConfigAndDefaults(furyctlPath, defaultPath)
+			if err != nil {
+				return err
+			}
+			if !debug {
+				defer cleanupTempDir(filepath.Base(defaultedFuryctlPath))
+			}
+
+			schema, err := santhosh.LoadSchema(schemaPath)
+			if err != nil {
+				return err
+			}
+
+			hasErrors := error(nil)
+			conf, err := yaml.FromFileV3[any](defaultedFuryctlPath)
+			if err != nil {
+				return err
+			}
+
+			if err := schema.ValidateInterface(conf); err != nil {
+				logrus.Debugf("Config file: %s", defaultedFuryctlPath)
+
+				fmt.Println(err)
+
+				hasErrors = errHasValidationErrors
+			}
+
+			if hasErrors == nil {
+				fmt.Println("Validation succeeded")
+			}
+
+			return hasErrors
+		},
+	}
+
+	cmd.Flags().StringP(
+		"config",
+		"c",
+		"furyctl.yaml",
+		"Path to the furyctl.yaml file",
+	)
+
+	cmd.Flags().StringP(
+		"distro-location",
+		"",
+		"",
+		"Base URL used to download schemas, defaults and the distribution manifest. "+
+			"It can either be a local path(eg: /path/to/fury/distribution) or "+
+			"a remote URL(eg: https://git@github.com/sighupio/fury-distribution?ref=BRANCH_NAME)."+
+			"Any format supported by hashicorp/go-getter can be used.",
+	)
+
+	return cmd
+}
+
+func mergeConfigAndDefaults(furyctlFilePath string, defaultsFilePath string) (string, error) {
+	defaultsFile, err := yaml.FromFileV2[map[any]any](defaultsFilePath)
+	if err != nil {
+		return "", fmt.Errorf("%w: %v", ErrYamlUnmarshalFile, err)
+	}
+
+	furyctlFile, err := yaml.FromFileV2[map[any]any](furyctlFilePath)
+	if err != nil {
+		return "", fmt.Errorf("%w: %v", ErrYamlUnmarshalFile, err)
+	}
+
+	defaultsModel := merge.NewDefaultModel(defaultsFile, ".data")
+	distributionModel := merge.NewDefaultModel(furyctlFile, ".spec.distribution")
+
+	distroMerger := merge.NewMerger(defaultsModel, distributionModel)
+
+	defaultedDistribution, err := distroMerger.Merge()
+	if err != nil {
+		return "", fmt.Errorf("%w: %v", ErrMergeDistroConfig, err)
+	}
+
+	furyctlModel := merge.NewDefaultModel(furyctlFile, ".spec.distribution")
+	defaultedDistributionModel := merge.NewDefaultModel(defaultedDistribution, ".data")
+
+	furyctlMerger := merge.NewMerger(furyctlModel, defaultedDistributionModel)
+
+	defaultedFuryctl, err := furyctlMerger.Merge()
+	if err != nil {
+		return "", fmt.Errorf("%w: %v", ErrMergeCompleteConfig, err)
+	}
+
+	outYaml, err := yaml.MarshalV2(defaultedFuryctl)
+	if err != nil {
+		return "", fmt.Errorf("%w: %v", ErrYamlMarshalFile, err)
+	}
+
+	outDirPath, err := os.MkdirTemp("", "furyctl-defaulted-")
+	if err != nil {
+		return "", fmt.Errorf("%w: %v", ErrCreatingTempDir, err)
+	}
+
+	confPath := filepath.Join(outDirPath, "config.yaml")
+	if err := os.WriteFile(confPath, outYaml, os.ModePerm); err != nil {
+		return "", fmt.Errorf("%w: %v", ErrWriteFile, err)
+	}
+
+	return confPath, nil
+}
