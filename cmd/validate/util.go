ed406186a4028169f09251407e6c22cd27087b9a
omissis <claudi0.beatric3@gmail.com>
2022-09-14 19:33:38 +0200

    feat: refactor validate command and add versions validation
    - refactor yaml package to handle v2 and v3
    - improve error handling in root command
    - refactor "validate" command to "validate config" command
    - handle version compantibility in "validate config" command
    - refactor common "validate config" logic to make it available for the "validate dependencies" command

--- cmd/validate/util.go
+++ cmd/validate/util.go
@@ -9,13 +9,9 @@ import (
 
 	"github.com/hashicorp/go-getter"
 	"github.com/sirupsen/logrus"
-	"gopkg.in/yaml.v2"
 
-	"github.com/sighupio/furyctl/internal/merge"
-	yaml2 "github.com/sighupio/furyctl/internal/yaml"
+	"github.com/sighupio/furyctl/internal/distribution"
 )
-
-const defaultBaseUrl = "https://git@github.com/sighupio/fury-distribution//%s?ref=feature/create-draft-of-the-furyctl-yaml-json-schema"
 
 var (
 	downloadProtocols = []string{"", "git::", "file::", "http::", "s3::", "gcs::", "mercurial::"}
@@ -33,111 +29,37 @@ var (
 	ErrYamlUnmarshalFile   = errors.New("error unmarshaling yaml file")
 )
 
-type FuryctlConfig struct {
-	ApiVersion string `yaml:"apiVersion"`
-	Kind       string `yaml:"kind"`
-	Spec       struct {
-		DistributionVersion string `yaml:"distributionVersion"`
-	} `yaml:"spec"`
-}
-
-func GetSchemaPath(basePath string, conf FuryctlConfig) string {
+func getSchemaPath(basePath string, conf distribution.FuryctlConfig) string {
 	avp := strings.Split(conf.ApiVersion, "/")
 	ns := strings.Replace(avp[0], ".sighup.io", "", 1)
 	ver := avp[1]
 	filename := fmt.Sprintf("%s-%s-%s.json", strings.ToLower(conf.Kind), ns, ver)
 
-	return filepath.Join(basePath, conf.Spec.DistributionVersion, filename)
+	return filepath.Join(basePath, "schemas", filename)
 }
 
-func GetDefaultPath(basePath string, conf FuryctlConfig) string {
-	return filepath.Join(basePath, conf.Spec.DistributionVersion, "furyctl-defaults.yaml")
+func getDefaultPath(basePath string) string {
+	return filepath.Join(basePath, "furyctl-defaults.yaml")
 }
 
-func DownloadFolder(distroLocation string, name string) (string, error) {
-	src := fmt.Sprintf(defaultBaseUrl, name)
-	if distroLocation != "" {
-		src = distroLocation
-	}
-
-	dir, err := os.MkdirTemp("", fmt.Sprintf("furyctl-%s-", name))
+func downloadDirectory(src string) (string, error) {
+	baseDst, err := os.MkdirTemp("", "furyctl-")
 	if err != nil {
 		return "", fmt.Errorf("%w: %v", ErrCreatingTempDir, err)
 	}
 
-	logrus.Debugf("Downloading '%s' from '%s' in '%s'", name, src, dir)
+	dst := filepath.Join(baseDst, "data")
 
-	if err := clientGet(src, dir); err != nil {
+	logrus.Debugf("Downloading '%s' in '%s'", src, dst)
+
+	if err := clientGet(src, dst); err != nil {
 		return "", fmt.Errorf("%w '%s': %v", ErrDownloadingFolder, src, err)
 	}
 
-	return dir, nil
+	return dst, nil
 }
 
-func MergeConfigAndDefaults(furyctlFilePath string, defaultsFilePath string) (string, error) {
-	defaultsFile, err := yaml2.FromFile[map[any]any](defaultsFilePath)
-	if err != nil {
-		return "", fmt.Errorf("%w: %v", ErrYamlUnmarshalFile, err)
-	}
-
-	furyctlFile, err := yaml2.FromFile[map[any]any](furyctlFilePath)
-	if err != nil {
-		return "", fmt.Errorf("%w: %v", ErrYamlUnmarshalFile, err)
-	}
-
-	defaultsModel := merge.NewDefaultModel(defaultsFile, ".data")
-	distributionModel := merge.NewDefaultModel(furyctlFile, ".spec.distribution")
-
-	distroMerger := merge.NewMerger(defaultsModel, distributionModel)
-
-	defaultedDistribution, err := distroMerger.Merge()
-	if err != nil {
-		return "", fmt.Errorf("%w: %v", ErrMergeDistroConfig, err)
-	}
-
-	furyctlModel := merge.NewDefaultModel(furyctlFile, ".spec.distribution")
-	defaultedDistributionModel := merge.NewDefaultModel(defaultedDistribution, ".data")
-
-	furyctlMerger := merge.NewMerger(furyctlModel, defaultedDistributionModel)
-
-	defaultedFuryctl, err := furyctlMerger.Merge()
-	if err != nil {
-		return "", fmt.Errorf("%w: %v", ErrMergeCompleteConfig, err)
-	}
-
-	outYaml, err := yaml.Marshal(defaultedFuryctl)
-	if err != nil {
-		return "", fmt.Errorf("%w: %v", ErrYamlMarshalFile, err)
-	}
-
-	outDirPath, err := os.MkdirTemp("", "furyctl-defaulted-")
-	if err != nil {
-		return "", fmt.Errorf("%w: %v", ErrCreatingTempDir, err)
-	}
-
-	confPath := filepath.Join(outDirPath, "config.yaml")
-	if err := os.WriteFile(confPath, outYaml, os.ModePerm); err != nil {
-		return "", fmt.Errorf("%w: %v", ErrWriteFile, err)
-	}
-
-	return confPath, nil
-}
-
-func PrintSummary(hasErrors bool) {
-	if hasErrors {
-		fmt.Println("Validation failed")
-	} else {
-		fmt.Println("Validation succeeded")
-	}
-}
-
-func PrintResults(err error, configFile string) {
-	fmt.Printf("CONFIG FILE %s\n", configFile)
-
-	fmt.Println(err)
-}
-
-func CleanupTempDir(dir string) {
+func cleanupTempDir(dir string) {
 	if err := os.RemoveAll(dir); err != nil {
 		if !errors.Is(err, os.ErrNotExist) {
 			logrus.Error(err)
@@ -163,11 +85,12 @@ func clientGet(src, dst string) error {
 			Mode: getter.ClientModeDir,
 		}
 
-		if err := client.Get(); err == nil {
-			logrus.Debug("Download successful")
-
+		err := client.Get()
+		if err == nil {
 			return nil
 		}
+
+		logrus.Debug(err)
 	}
 
 	return errDownloadOptionsExausted
@@ -176,7 +99,7 @@ func clientGet(src, dst string) error {
 // urlHasForcedProtocol checks if the url has a forced protocol as described in hashicorp/go-getter.
 func urlHasForcedProtocol(url string) bool {
 	for _, dp := range downloadProtocols {
-		if strings.HasPrefix(url, dp) {
+		if dp != "" && strings.HasPrefix(url, dp) {
 			return true
 		}
 	}
