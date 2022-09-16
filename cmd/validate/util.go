8d1b5b192d7eccd2fe7b2634c589084b3d5016a1
Alessio Pragliola <alessio.pragliola@sighup.io>
2022-09-16 12:43:34 +0200

    feat: added unit tests to validate/util

--- cmd/validate/util.go
+++ cmd/validate/util.go
@@ -29,13 +29,23 @@ var (
 	ErrYamlUnmarshalFile   = errors.New("error unmarshaling yaml file")
 )
 
-func getSchemaPath(basePath string, conf distribution.FuryctlConfig) string {
+func getSchemaPath(basePath string, conf distribution.FuryctlConfig) (string, error) {
 	avp := strings.Split(conf.ApiVersion, "/")
+
+	if len(avp) < 2 {
+		return "", fmt.Errorf("invalid apiVersion: %s", conf.ApiVersion)
+	}
+
 	ns := strings.Replace(avp[0], ".sighup.io", "", 1)
 	ver := avp[1]
+
+	if conf.Kind == "" {
+		return "", fmt.Errorf("kind is empty")
+	}
+
 	filename := fmt.Sprintf("%s-%s-%s.json", strings.ToLower(conf.Kind), ns, ver)
 
-	return filepath.Join(basePath, "schemas", filename)
+	return filepath.Join(basePath, "schemas", filename), nil
 }
 
 func getDefaultPath(basePath string) string {
