e72676505520546fc0d1cb36ba7d2ae9efac5500
omissis <claudi0.beatric3@gmail.com>
2022-09-20 11:03:53 +0200

    chore: add missing license banners

--- cmd/validate/util_test.go
+++ cmd/validate/util_test.go
@@ -1,12 +1,17 @@
+// Copyright (c) 2017-present SIGHUP s.r.l All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
 package validate
 
 import (
 	"fmt"
-	"github.com/sighupio/furyctl/internal/distribution"
-	"github.com/sighupio/furyctl/internal/semver"
 	"os"
 	"path/filepath"
 	"testing"
+
+	"github.com/sighupio/furyctl/internal/distribution"
+	"github.com/sighupio/furyctl/internal/semver"
 )
 
 func TestGetSchemaPath(t *testing.T) {
