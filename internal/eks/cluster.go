c78dee492b4d7f5893263cfed4c16f3a6731eaff
Alessio Pragliola <alessio.pragliola@sighup.io>
2022-09-23 17:31:14 +0200

    fix: missing folder creation

--- internal/eks/cluster.go
+++ internal/eks/cluster.go
@@ -13,17 +13,24 @@ import (
 var ErrUnsupportedApiVersion = errors.New("unsupported api version")
 
 type ClusterCreator interface {
-	WithPhase(phase string) ClusterCreator
-	WithKfdManifest(kfdManifest distribution.Manifest) ClusterCreator
-	WithConfigPath(configPath string) ClusterCreator
-	WithVpnAutoConnect(vpnAutoConnect bool) ClusterCreator
 	Create() error
 }
 
-func NewClusterCreator(apiVersion string) (ClusterCreator, error) {
+func NewClusterCreator(
+	apiVersion string,
+	phase string,
+	kfdManifest distribution.Manifest,
+	configPath string,
+	vpnAutoConnect bool,
+) (ClusterCreator, error) {
 	switch apiVersion {
 	case "kfd.sighup.io/v1alpha2":
-		return &V1alpha2{}, nil
+		return &V1alpha2{
+			Phase:          phase,
+			KfdManifest:    kfdManifest,
+			ConfigPath:     configPath,
+			VpnAutoConnect: vpnAutoConnect,
+		}, nil
 	}
 
 	return nil, ErrUnsupportedApiVersion
