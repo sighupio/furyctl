24517ebf2c007ce58febe9a7503cb043676b1d64
omissis <claudi0.beatric3@gmail.com>
2022-09-29 14:22:15 +0200

    feat: tag all test files with build tag, separate integration test data from e2e's

--- internal/distribution/download.go
+++ internal/distribution/download.go
@@ -9,7 +9,6 @@ import (
 	"fmt"
 	"os"
 	"path/filepath"
-	"strings"
 
 	"github.com/go-playground/validator/v10"
 	"github.com/sirupsen/logrus"
@@ -123,27 +122,4 @@ func (d *Downloader) Download(
 		MinimalConf:    minimalConf,
 		DistroManifest: kfdManifest,
 	}, nil
-}
-
-func GetSchemaPath(basePath string, conf config.Furyctl) (string, error) {
-	avp := strings.Split(conf.ApiVersion, "/")
-
-	if len(avp) < 2 {
-		return "", fmt.Errorf("invalid apiVersion: %s", conf.ApiVersion)
-	}
-
-	ns := strings.Replace(avp[0], ".sighup.io", "", 1)
-	ver := avp[1]
-
-	if conf.Kind == "" {
-		return "", fmt.Errorf("kind is empty")
-	}
-
-	filename := fmt.Sprintf("%s-%s-%s.json", strings.ToLower(conf.Kind), ns, ver)
-
-	return filepath.Join(basePath, "schemas", filename), nil
-}
-
-func GetDefaultsPath(basePath string) string {
-	return filepath.Join(basePath, "furyctl-defaults.yaml")
 }
