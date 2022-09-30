4987bb48e3bc2bb0d4ebebe54f5070127c0f6c6d
omissis <claudi0.beatric3@gmail.com>
2022-09-30 21:12:28 +0200

    chore: remove pointless use of generics

--- internal/apis/kfd/v1alpha2/eks/init.go
+++ internal/apis/kfd/v1alpha2/eks/init.go
@@ -1,3 +1,7 @@
+// Copyright (c) 2017-present SIGHUP s.r.l All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
 package eks
 
 import "github.com/sighupio/furyctl/internal/cluster"
@@ -6,7 +10,7 @@ func init() {
 	cluster.RegisterCreatorFactory(
 		"kfd.sighup.io/v1alpha2",
 		"ekscluster",
-		func(opts []cluster.CreatorOption[any]) cluster.Creator {
+		func(opts []cluster.CreatorOption) cluster.Creator {
 			cc := &ClusterCreator{}
 			cc.SetOptions(opts)
 
