4efb032c8b25a18ab7819b85537b32cabbf20f54
Luigi Barbato <luigi.barbato@sighup.io>
2022-09-30 12:36:00 +0200

    chore: add missing license banner, fumpt and fmt format

--- internal/app/update.go
+++ internal/app/update.go
@@ -1,6 +1,11 @@
+// Copyright (c) 2017-present SIGHUP s.r.l All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
 package app
 
 import (
+	"context"
 	"encoding/json"
 	"net/http"
 )
@@ -23,7 +28,13 @@ func NewUpdate(furyctlBinVersion string) *Update {
 func (u *Update) FetchLastRelease() (Release, error) {
 	var release Release
 
-	resp, err := http.Get("https://api.github.com/repos/sighupio/furyctl/releases/latest")
+	ctx := context.Background()
+	req, err := http.NewRequestWithContext(ctx, http.MethodGet, "https://api.github.com/repos/sighupio/furyctl/releases/latest", nil)
+	if err != nil {
+		return release, err
+	}
+
+	resp, err := http.DefaultClient.Do(req)
 	if err != nil {
 		return release, err
 	}
