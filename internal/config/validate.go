3c78d001fd253b582ab5dde39355738cdb1b4125
omissis <claudi0.beatric3@gmail.com>
2022-12-22 16:55:41 +0100

    chore: remove unused code

--- internal/config/validate.go
+++ internal/config/validate.go
@@ -13,7 +13,6 @@ import (
 	"github.com/sighupio/fury-distribution/pkg/config"
 	"github.com/sighupio/furyctl/internal/analytics"
 	"github.com/sighupio/furyctl/internal/distribution"
-	"github.com/sighupio/furyctl/internal/merge"
 	"github.com/sighupio/furyctl/internal/schema/santhosh"
 	iox "github.com/sighupio/furyctl/internal/x/io"
 	yamlx "github.com/sighupio/furyctl/internal/x/yaml"
@@ -136,59 +135,4 @@ func loadFromFile(path string) (config.Furyctl, error) {
 	}
 
 	return conf, err
-}
-
-func mergeWithDefaults(furyctlConfPath, defaultsConfPath string) (string, error) {
-	defaultsFile, err := yamlx.FromFileV2[map[any]any](defaultsConfPath)
-	if err != nil {
-		return "", fmt.Errorf("%w: %v", distribution.ErrYamlUnmarshalFile, err)
-	}
-
-	furyctlFile, err := yamlx.FromFileV2[map[any]any](furyctlConfPath)
-	if err != nil {
-		return "", fmt.Errorf("%w: %v", distribution.ErrYamlUnmarshalFile, err)
-	}
-
-	defaultsModel := merge.NewDefaultModel(defaultsFile, ".data")
-	distributionModel := merge.NewDefaultModel(furyctlFile, ".spec.distribution")
-
-	distroMerger := merge.NewMerger(defaultsModel, distributionModel)
-
-	defaultedDistribution, err := distroMerger.Merge()
-	if err != nil {
-		return "", fmt.Errorf("%w: %v", distribution.ErrMergeDistroConfig, err)
-	}
-
-	furyctlModel := merge.NewDefaultModel(furyctlFile, ".spec.distribution")
-	defaultedDistributionModel := merge.NewDefaultModel(defaultedDistribution, ".data")
-
-	furyctlMerger := merge.NewMerger(furyctlModel, defaultedDistributionModel)
-
-	defaultedFuryctl, err := furyctlMerger.Merge()
-	if err != nil {
-		return "", fmt.Errorf("%w: %v", distribution.ErrMergeCompleteConfig, err)
-	}
-
-	outYaml, err := yamlx.MarshalV2(defaultedFuryctl)
-	if err != nil {
-		return "", fmt.Errorf("%w: %v", distribution.ErrYamlMarshalFile, err)
-	}
-
-	outDirPath, err := os.MkdirTemp("", "furyctl-defaulted-")
-	if err != nil {
-		return "", fmt.Errorf("%w: %v", distribution.ErrCreatingTempDir, err)
-	}
-
-	confPath := filepath.Join(outDirPath, "config.yaml")
-	if err := os.WriteFile(confPath, outYaml, os.ModePerm); err != nil {
-		return "", fmt.Errorf("%w: %v", distribution.ErrWriteFile, err)
-	}
-
-	return confPath, nil
-}
-
-func checkError(err error) {
-	if err != nil {
-		panic(err)
-	}
 }
