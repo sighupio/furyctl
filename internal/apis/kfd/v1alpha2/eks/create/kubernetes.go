18a052685c972fbbb122e1f8d6761600937b8bdd
Alessio Pragliola <alessio.pragliola@sighup.io>
2022-12-23 09:43:13 +0100

    chore: better error msg vpcID

--- internal/apis/kfd/v1alpha2/eks/create/kubernetes.go
+++ internal/apis/kfd/v1alpha2/eks/create/kubernetes.go
@@ -36,7 +36,7 @@ var (
 	errVpcCIDRFromOut     = errors.New("cannot read vpc_cidr_block from infrastructure's output.json")
 	errVpcCIDRNotFound    = errors.New("vpc_cidr_block not found in infra output")
 	errVpcIDNotFound      = errors.New(
-		"vpc_id not found in kubernetes spec or something went wrong in infrastructure phase",
+		"vpc id not found: you forgot to specify one or the infrastructure phase failed",
 	)
 )
 
