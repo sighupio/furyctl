84fb7bf74eca4af93c4da20577c9be28dbfca09d
Alessio Pragliola <83355398+Al-Pragliola@users.noreply.github.com>
2023-02-23 16:57:19 +0100

    Feat: added terraform state bucket validation (#201)
    * feat: added tools configuration validation
    
    chore: added dummy bucket config
    
    fix: e2e tests
    
    fix: test e2e check CI
    
    fix: CI cannot create/delete buckets in parallel
    
    fix: CI drone s3 bucket creation conditional
    
    feat: added rich semver handling
    
    * feat: switched to terraform syntax in semver
    
    fix: unit tests, removed unused func call
    
    chore: update all awscli version to wildcard
    
    chore: update fury-distribution dep
    
    chore: update again dep to fury-distribution
    
    chore: upgrade fury-distribution dep
    
    chore: removed dummy
    
    * fix: switched to correct s3 bucket api
    
    * fix: check if bucket exists in drone
    
    Co-authored-by: Claudio Beatrice <claudi0.beatric3@gmail.com>
    
    * chore: apply suggestions from code review
    
    Co-authored-by: Claudio Beatrice <claudi0.beatric3@gmail.com>
    
    * chore: rename Get* to New*
    
    ---------
    
    Co-authored-by: Claudio Beatrice <claudi0.beatric3@gmail.com>

--- internal/semver/compare_test.go
+++ internal/semver/compare_test.go
@@ -9,12 +9,12 @@ package semver_test
 import (
 	"testing"
 
-	"github.com/google/go-cmp/cmp"
-
 	"github.com/sighupio/furyctl/internal/semver"
 )
 
-func Test_NewVersion(t *testing.T) {
+func TestGetVersion(t *testing.T) {
+	t.Parallel()
+
 	tests := []struct {
 		name    string
 		version string
@@ -46,14 +46,14 @@ func Test_NewVersion(t *testing.T) {
 			wantErr: false,
 		},
 		{
-			name:    "invalid v-less version",
+			name:    "valid v-less version",
 			version: "1.2.3",
-			wantErr: true,
+			wantErr: false,
 		},
 		{
-			name:    "invalid numeric version",
+			name:    "valid numeric version",
 			version: "11",
-			wantErr: true,
+			wantErr: false,
 		},
 		{
 			name:    "invalid string version",
@@ -61,295 +61,79 @@ func Test_NewVersion(t *testing.T) {
 			wantErr: true,
 		},
 	}
+
 	for _, tt := range tests {
+		tt := tt
+
 		t.Run(tt.name, func(t *testing.T) {
+			t.Parallel()
+
 			_, err := semver.NewVersion(tt.version)
 			if (err != nil) != tt.wantErr {
 				t.Errorf("NewVersion() error = %v, wantErr %v", err, tt.wantErr)
+
 				return
 			}
 		})
 	}
 }
 
-func TestGt(t *testing.T) {
+func TestGetConstraint(t *testing.T) {
 	t.Parallel()
 
-	testCases := []struct {
-		desc string
-		a    string
-		b    string
-		want bool
+	tests := []struct {
+		name       string
+		constraint string
+		wantErr    bool
 	}{
 		{
-			desc: "a equals b",
-			a:    "0.1.0",
-			b:    "0.1.0",
-			want: false,
+			name:       "valid constraint",
+			constraint: "v1.2.3",
+			wantErr:    false,
 		},
 		{
-			desc: "a lesser patch than b",
-			a:    "0.1.0",
-			b:    "0.1.1",
-			want: false,
+			name:       "valid v-less constraint",
+			constraint: "1.2.3",
+			wantErr:    false,
 		},
 		{
-			desc: "a greater patch than b",
-			a:    "0.1.1",
-			b:    "0.1.0",
-			want: true,
+			name:       "valid constraint with greater than",
+			constraint: ">1.2.3",
+			wantErr:    false,
 		},
 		{
-			desc: "a lesser minor than b",
-			a:    "0.1.0",
-			b:    "0.2.0",
-			want: false,
+			name:       "valid constraint with greater or equal than",
+			constraint: ">=1.2.3",
+			wantErr:    false,
 		},
 		{
-			desc: "a greater minor than b",
-			a:    "0.2.0",
-			b:    "0.1.0",
-			want: true,
+			name:       "valid constraint with pessimistic",
+			constraint: "~>1.2.3",
+			wantErr:    false,
 		},
 		{
-			desc: "a lesser major than b",
-			a:    "0.1.0",
-			b:    "1.1.0",
-			want: false,
+			name:       "valid constraint with wildcard",
+			constraint: "*",
+			wantErr:    false,
 		},
 		{
-			desc: "a greater major than b",
-			a:    "1.1.0",
-			b:    "0.1.0",
-			want: true,
-		},
-		{
-			desc: "a lesser major, greater minor and patch than b",
-			a:    "0.2.2",
-			b:    "1.0.0",
-			want: false,
-		},
-		{
-			desc: "a lesser minor, greater patch than b",
-			a:    "0.2.2",
-			b:    "0.3.0",
-			want: false,
+			name:       "invalid constraint",
+			constraint: ">1.",
+			wantErr:    true,
 		},
 	}
-	for _, tC := range testCases {
-		tC := tC
 
-		t.Run(tC.desc, func(t *testing.T) {
+	for _, tt := range tests {
+		tt := tt
+
+		t.Run(tt.name, func(t *testing.T) {
 			t.Parallel()
 
-			if got := semver.Gt(tC.a, tC.b); got != tC.want {
-				t.Errorf("Gt() = %v, want %v", got, tC.want)
-			}
-		})
-	}
-}
+			_, err := semver.NewConstraint(tt.constraint)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("NewConstraint() error = %v, wantErr %v", err, tt.wantErr)
 
-func TestParts(t *testing.T) {
-	t.Parallel()
-
-	testCases := []struct {
-		desc    string
-		version string
-		want    []any
-	}{
-		{desc: "0.0.4", want: []any{0, 0, 4, ""}},
-		{desc: "1.2.3", want: []any{1, 2, 3, ""}},
-		{desc: "10.20.30", want: []any{10, 20, 30, ""}},
-		{desc: "1.1.2-prerelease+meta", want: []any{1, 1, 2, "prerelease+meta"}},
-		{desc: "1.1.2+meta", want: []any{1, 1, 2, "meta"}},
-		{desc: "1.1.2+meta-valid", want: []any{1, 1, 2, "meta-valid"}},
-		{desc: "1.0.0-alpha", want: []any{1, 0, 0, "alpha"}},
-		{desc: "1.0.0-beta", want: []any{1, 0, 0, "beta"}},
-		{desc: "1.0.0-alpha.beta", want: []any{1, 0, 0, "alpha.beta"}},
-		{desc: "1.0.0-alpha.beta.1", want: []any{1, 0, 0, "alpha.beta.1"}},
-		{desc: "1.0.0-alpha.1", want: []any{1, 0, 0, "alpha.1"}},
-		{desc: "1.0.0-alpha0.valid", want: []any{1, 0, 0, "alpha0.valid"}},
-		{desc: "1.0.0-alpha.0valid", want: []any{1, 0, 0, "alpha.0valid"}},
-		{desc: "1.0.0-alpha-a.b-c-somethinglong+build.1-aef.1-its-okay", want: []any{1, 0, 0, "alpha-a.b-c-somethinglong+build.1-aef.1-its-okay"}},
-		{desc: "1.0.0-rc.1+build.1", want: []any{1, 0, 0, "rc.1+build.1"}},
-		{desc: "2.0.0-rc.1+build.123", want: []any{2, 0, 0, "rc.1+build.123"}},
-		{desc: "1.2.3-beta", want: []any{1, 2, 3, "beta"}},
-		{desc: "10.2.3-DEV-SNAPSHOT", want: []any{10, 2, 3, "DEV-SNAPSHOT"}},
-		{desc: "1.2.3-SNAPSHOT-123", want: []any{1, 2, 3, "SNAPSHOT-123"}},
-		{desc: "1.0.0", want: []any{1, 0, 0, ""}},
-		{desc: "2.0.0", want: []any{2, 0, 0, ""}},
-		{desc: "1.1.7", want: []any{1, 1, 7, ""}},
-		{desc: "2.0.0+build.1848", want: []any{2, 0, 0, "build.1848"}},
-		{desc: "2.0.1-alpha.1227", want: []any{2, 0, 1, "alpha.1227"}},
-		{desc: "1.0.0-alpha+beta", want: []any{1, 0, 0, "alpha+beta"}},
-		{desc: "1.2.3----RC-SNAPSHOT.12.9.1--.12+788", want: []any{1, 2, 3, "---RC-SNAPSHOT.12.9.1--.12+788"}},
-		{desc: "1.2.3----R-S.12.9.1--.12+meta", want: []any{1, 2, 3, "---R-S.12.9.1--.12+meta"}},
-		{desc: "1.2.3----RC-SNAPSHOT.12.9.1--.12", want: []any{1, 2, 3, "---RC-SNAPSHOT.12.9.1--.12"}},
-		{desc: "1.0.0+0.build.1-rc.10000aaa-kk-0.1", want: []any{1, 0, 0, "0.build.1-rc.10000aaa-kk-0.1"}},
-		{desc: "999999999999999999.999999999999999999.99999999999999999", want: []any{999999999999999999, 999999999999999999, 99999999999999999, ""}},
-		{desc: "1.0.0-0A.is.legal", want: []any{1, 0, 0, "0A.is.legal"}},
-
-		{desc: "1", want: []any{0, 0, 0, ""}},
-		{desc: "1.2", want: []any{0, 0, 0, ""}},
-		{desc: "1.2.3-0123", want: []any{0, 0, 0, ""}},
-		{desc: "1.2.3-0123.0123", want: []any{0, 0, 0, ""}},
-		{desc: "1.1.2+.123", want: []any{0, 0, 0, ""}},
-		{desc: "+invalid", want: []any{0, 0, 0, ""}},
-		{desc: "-invalid", want: []any{0, 0, 0, ""}},
-		{desc: "-invalid+invalid", want: []any{0, 0, 0, ""}},
-		{desc: "-invalid.01", want: []any{0, 0, 0, ""}},
-		{desc: "alpha", want: []any{0, 0, 0, ""}},
-		{desc: "alpha.beta", want: []any{0, 0, 0, ""}},
-		{desc: "alpha.beta.1", want: []any{0, 0, 0, ""}},
-		{desc: "alpha.1", want: []any{0, 0, 0, ""}},
-		{desc: "alpha+beta", want: []any{0, 0, 0, ""}},
-		{desc: "alpha_beta", want: []any{0, 0, 0, ""}},
-		{desc: "alpha.", want: []any{0, 0, 0, ""}},
-		{desc: "alpha..", want: []any{0, 0, 0, ""}},
-		{desc: "beta", want: []any{0, 0, 0, ""}},
-		{desc: "1.0.0-alpha_beta", want: []any{0, 0, 0, ""}},
-		{desc: "-alpha.", want: []any{0, 0, 0, ""}},
-		{desc: "1.0.0-alpha..", want: []any{0, 0, 0, ""}},
-		{desc: "1.0.0-alpha..1", want: []any{0, 0, 0, ""}},
-		{desc: "1.0.0-alpha...1", want: []any{0, 0, 0, ""}},
-		{desc: "1.0.0-alpha....1", want: []any{0, 0, 0, ""}},
-		{desc: "1.0.0-alpha.....1", want: []any{0, 0, 0, ""}},
-		{desc: "1.0.0-alpha......1", want: []any{0, 0, 0, ""}},
-		{desc: "1.0.0-alpha.......1", want: []any{0, 0, 0, ""}},
-		{desc: "01.1.1", want: []any{0, 0, 0, ""}},
-		{desc: "1.01.1", want: []any{0, 0, 0, ""}},
-		{desc: "1.1.01", want: []any{0, 0, 0, ""}},
-		{desc: "1.2", want: []any{0, 0, 0, ""}},
-		{desc: "1.2.3.DEV", want: []any{0, 0, 0, ""}},
-		{desc: "1.2-SNAPSHOT", want: []any{0, 0, 0, ""}},
-		{desc: "1.2.31.2.3----RC-SNAPSHOT.12.09.1--..12+788", want: []any{0, 0, 0, ""}},
-		{desc: "1.2-RC-SNAPSHOT", want: []any{0, 0, 0, ""}},
-		{desc: "-1.0.3-gamma+b7718", want: []any{0, 0, 0, ""}},
-		{desc: "+justmeta", want: []any{0, 0, 0, ""}},
-		{desc: "9.8.7+meta+meta", want: []any{0, 0, 0, ""}},
-		{desc: "9.8.7-whatever+meta+meta", want: []any{0, 0, 0, ""}},
-		{desc: "99999999999999999999999.999999999999999999.99999999999999999----RC-SNAPSHOT.12.09.1--------------------------------..12", want: []any{0, 0, 0, ""}},
-	}
-	for _, tC := range testCases {
-		tC := tC
-
-		t.Run(tC.desc, func(t *testing.T) {
-			t.Parallel()
-
-			major, minor, patch, rest := semver.Parts(tC.desc)
-
-			parts := []any{major, minor, patch, rest}
-
-			if !cmp.Equal(parts, tC.want) {
-				t.Errorf("parts mismatch (-want +got):\n%s", cmp.Diff(tC.want, parts))
-			}
-		})
-	}
-}
-
-func TestSamePatchStr(t *testing.T) {
-	t.Parallel()
-
-	testCases := []struct {
-		desc string
-		a    string
-		b    string
-		want bool
-	}{
-		{
-			desc: "same patch, different build using dash",
-			a:    "v1.2.3-1",
-			b:    "v1.2.3-2",
-			want: true,
-		},
-		{
-			desc: "same patch, different build using plus",
-			a:    "v1.2.3+b1",
-			b:    "v1.2.3+b2",
-			want: true,
-		},
-		{
-			desc: "same patch",
-			a:    "v1.2.3",
-			b:    "v1.2.3",
-			want: true,
-		},
-		{
-			desc: "same minor",
-			a:    "v1.2.3",
-			b:    "v1.2.4",
-			want: false,
-		},
-		{
-			desc: "same major",
-			a:    "v1.2.3",
-			b:    "v1.3.4",
-			want: false,
-		},
-	}
-	for _, tC := range testCases {
-		tC := tC
-
-		t.Run(tC.desc, func(t *testing.T) {
-			t.Parallel()
-
-			got := semver.SamePatchStr(tC.a, tC.b)
-			if got != tC.want {
-				t.Errorf("want = %t, got = %t", tC.want, got)
-			}
-		})
-	}
-}
-
-func TesSameMinorStr(t *testing.T) {
-	t.Parallel()
-
-	testCases := []struct {
-		desc string
-		a    string
-		b    string
-		want bool
-	}{
-		{
-			desc: "same patch, different build using dash",
-			a:    "v1.2.3-1",
-			b:    "v1.2.3-2",
-			want: true,
-		},
-		{
-			desc: "same patch, different build using plus",
-			a:    "v1.2.3+b1",
-			b:    "v1.2.3+b2",
-			want: true,
-		},
-		{
-			desc: "same patch",
-			a:    "v1.2.3",
-			b:    "v1.2.3",
-			want: true,
-		},
-		{
-			desc: "same minor",
-			a:    "v1.2.3",
-			b:    "v1.2.4",
-			want: true,
-		},
-		{
-			desc: "same major",
-			a:    "v1.2.3",
-			b:    "v1.3.4",
-			want: false,
-		},
-	}
-	for _, tC := range testCases {
-		tC := tC
-
-		t.Run(tC.desc, func(t *testing.T) {
-			t.Parallel()
-
-			got := semver.SameMinorStr(tC.a, tC.b)
-			if got != tC.want {
-				t.Errorf("want = %t, got = %t", tC.want, got)
+				return
 			}
 		})
 	}
