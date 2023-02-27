9dfcfea958cadfb0d3030dc543af29df20c8b609
Alessio Pragliola <alessio.pragliola@sighup.io>
2023-02-27 12:16:36 +0100

    feat: split dump template logic

--- cmd/dump/template.go
+++ cmd/dump/template.go
@@ -7,8 +7,6 @@ package dump
 import (
 	"errors"
 	"fmt"
-	"os"
-	"path/filepath"
 
 	"github.com/sirupsen/logrus"
 	"github.com/spf13/cobra"
@@ -17,23 +15,12 @@ import (
 	"github.com/sighupio/furyctl/internal/cmd/cmdutil"
 	"github.com/sighupio/furyctl/internal/config"
 	"github.com/sighupio/furyctl/internal/distribution"
-	"github.com/sighupio/furyctl/internal/merge"
-	"github.com/sighupio/furyctl/internal/template"
 	cobrax "github.com/sighupio/furyctl/internal/x/cobra"
 	netx "github.com/sighupio/furyctl/internal/x/net"
 	yamlx "github.com/sighupio/furyctl/internal/x/yaml"
 )
 
-const (
-	source           = "templates/distribution"
-	suffix           = ".tpl"
-	defaultsFileName = "furyctl-defaults.yaml"
-)
-
-var (
-	ErrSourceDirDoesNotExist = errors.New("source directory does not exist")
-	ErrParsingFlag           = errors.New("error while parsing flag")
-)
+var ErrParsingFlag = errors.New("error while parsing flag")
 
 type TemplateCmdFlags struct {
 	DryRun         bool
@@ -86,16 +73,6 @@ The generated folder will be created starting from a provided template and the p
 				return fmt.Errorf("error while validating configuration file: %w", err)
 			}
 
-			defaultsFilePath := filepath.Join(res.RepoPath, defaultsFileName)
-
-			distributionFile, err := yamlx.FromFileV2[map[any]any](defaultsFilePath)
-			if err != nil {
-				cmdEvent.AddErrorMessage(err)
-				tracker.Track(cmdEvent)
-
-				return fmt.Errorf("%s - %w", defaultsFilePath, err)
-			}
-
 			furyctlFile, err := yamlx.FromFileV2[map[any]any](flags.FuryctlPath)
 			if err != nil {
 				cmdEvent.AddErrorMessage(err)
@@ -104,110 +81,26 @@ The generated folder will be created starting from a provided template and the p
 				return fmt.Errorf("%s - %w", flags.FuryctlPath, err)
 			}
 
-			sourcePath := filepath.Join(res.RepoPath, source)
+			logrus.Info("Generating distribution manifests...")
 
-			if _, err := os.Stat(sourcePath); os.IsNotExist(err) {
-				cmdEvent.AddErrorMessage(ErrSourceDirDoesNotExist)
-				tracker.Track(cmdEvent)
-
-				return ErrSourceDirDoesNotExist
-			}
-
-			merger := merge.NewMerger(
-				merge.NewDefaultModel(distributionFile, ".data"),
-				merge.NewDefaultModel(furyctlFile, ".spec.distribution"),
-			)
-
-			_, err = merger.Merge()
-			if err != nil {
-				cmdEvent.AddErrorMessage(err)
-				tracker.Track(cmdEvent)
-
-				return fmt.Errorf("error merging files: %w", err)
-			}
-
-			reverseMerger := merge.NewMerger(
-				*merger.GetCustom(),
-				*merger.GetBase(),
-			)
-
-			_, err = reverseMerger.Merge()
-			if err != nil {
-				cmdEvent.AddErrorMessage(err)
-				tracker.Track(cmdEvent)
-
-				return fmt.Errorf("error merging files: %w", err)
-			}
-
-			tmplCfg, err := template.NewConfig(reverseMerger, reverseMerger, []string{})
-			if err != nil {
-				cmdEvent.AddErrorMessage(err)
-				tracker.Track(cmdEvent)
-
-				return fmt.Errorf("error creating template config: %w", err)
-			}
-
-			outYaml, err := yamlx.MarshalV2(tmplCfg)
-			if err != nil {
-				cmdEvent.AddErrorMessage(err)
-				tracker.Track(cmdEvent)
-
-				return fmt.Errorf("error marshaling template config: %w", err)
-			}
-
-			outDirPath, err := os.MkdirTemp("", "furyctl-dist-")
-			if err != nil {
-				cmdEvent.AddErrorMessage(err)
-				tracker.Track(cmdEvent)
-
-				return fmt.Errorf("error creating temporary directory: %w", err)
-			}
-
-			confPath := filepath.Join(outDirPath, "config.yaml")
-
-			logrus.Debugf("config path = %s", confPath)
-
-			if err = os.WriteFile(confPath, outYaml, os.ModePerm); err != nil {
-				cmdEvent.AddErrorMessage(err)
-				tracker.Track(cmdEvent)
-
-				return fmt.Errorf("error writing config file: %w", err)
-			}
-
-			if !flags.NoOverwrite {
-				if err = os.RemoveAll(flags.OutDir); err != nil {
-					cmdEvent.AddErrorMessage(err)
-					tracker.Track(cmdEvent)
-
-					return fmt.Errorf("error removing target directory: %w", err)
-				}
-			}
-
-			templateModel, err := template.NewTemplateModel(
-				sourcePath,
+			distroManBuilder := distribution.NewManifestBuilder(
+				furyctlFile,
+				res.RepoPath,
 				flags.OutDir,
-				confPath,
-				outDirPath,
-				suffix,
 				flags.NoOverwrite,
 				flags.DryRun,
 			)
-			if err != nil {
+
+			if err := distroManBuilder.Build(); err != nil {
 				cmdEvent.AddErrorMessage(err)
 				tracker.Track(cmdEvent)
 
-				return fmt.Errorf("error creating template model: %w", err)
+				return fmt.Errorf("error while generating distribution manifests: %w", err)
 			}
 
-			err = templateModel.Generate()
-			if err != nil {
-				cmdEvent.AddErrorMessage(err)
-				tracker.Track(cmdEvent)
+			logrus.Info("Distribution manifests generated successfully")
 
-				return fmt.Errorf("error generating from template: %w", err)
-			}
-
-			cmdEvent.AddSuccessMessage("Distribution template generated successfully")
+			cmdEvent.AddSuccessMessage("Distribution manifests generated successfully")
 			tracker.Track(cmdEvent)
 
 			return nil
