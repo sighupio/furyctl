4a64ee1ffa0d30788f7e126e163fec0d08671ddb
omissis <claudi0.beatric3@gmail.com>
2023-05-03 16:05:59 +0200

    chore: bump fury-provisioners version

--- internal/cluster/provisioners/vsphere/provisioner.go
+++ internal/cluster/provisioners/vsphere/provisioner.go
@@ -346,7 +346,7 @@ func downloadAnsibleRoles(workingDirectory string) error {
 	}
 
 	client := &getter.Client{
-		Src:  "https://github.com/sighupio/furyctl-provisioners/archive/refs/tags/v0.6.3.zip//furyctl-provisioners-0.6.3/roles",
+		Src:  "https://github.com/sighupio/furyctl-provisioners/archive/refs/tags/v0.7.0.zip//furyctl-provisioners-0.7.0/roles",
 		Dst:  downloadPath,
 		Pwd:  workingDirectory,
 		Mode: getter.ClientModeAny,
