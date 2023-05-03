ac3cd2d1b03ead7d806b730f4d029802d1fdc98e
omissis <claudi0.beatric3@gmail.com>
2023-05-03 16:12:58 +0200

    chore: set fury-provisioners to rc for testing

--- internal/cluster/provisioners/vsphere/provisioner.go
+++ internal/cluster/provisioners/vsphere/provisioner.go
@@ -346,7 +346,7 @@ func downloadAnsibleRoles(workingDirectory string) error {
 	}
 
 	client := &getter.Client{
-		Src:  "https://github.com/sighupio/furyctl-provisioners/archive/refs/tags/v0.7.0.zip//furyctl-provisioners-0.7.0/roles",
+		Src:  "https://github.com/sighupio/furyctl-provisioners/archive/refs/tags/v0.7.0-rc.0.zip//furyctl-provisioners-0.7.0-rc.0/roles",
 		Dst:  downloadPath,
 		Pwd:  workingDirectory,
 		Mode: getter.ClientModeAny,
