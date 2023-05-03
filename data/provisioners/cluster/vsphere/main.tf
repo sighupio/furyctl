baf7aa07e3c397a9d751e1908978463dc0d03d05
omissis <claudi0.beatric3@gmail.com>
2023-05-03 18:44:51 +0200

    chore: set fury-provisioners to stable after testing

--- data/provisioners/cluster/vsphere/main.tf
+++ data/provisioners/cluster/vsphere/main.tf
@@ -44,7 +44,7 @@ locals {
 }
 
 module "fury" {
-  source = "https://github.com/sighupio/furyctl-provisioners/archive/refs/tags/v0.7.0-rc.0.zip//furyctl-provisioners-0.7.0-rc.0/modules/cluster/vsphere"
+  source = "https://github.com/sighupio/furyctl-provisioners/archive/refs/tags/v0.7.0.zip//furyctl-provisioners-0.7.0/modules/cluster/vsphere"
 
   name                        = var.name
   kube_version                = var.kube_version
