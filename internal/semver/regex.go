fcccc079fc1845b75b5a149e3a5e856ac45c9bca
Claudio Beatrice <claudi0.beatric3@gmail.com>
2023-06-27 16:09:44 +0200

    fix: add missing license banner

--- internal/semver/regex.go
+++ internal/semver/regex.go
@@ -1,3 +1,7 @@
+// Copyright (c) 2017-present SIGHUP s.r.l All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
 package semver
 
 const Regex string = `v?(?P<major>0|[1-9]\d*)\.(?P<minor>0|[1-9]\d*)\.(?P<patch>0|[1-9]\d*)(?:-(?P<prerelease>(?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*)(?:\.(?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*))*))?(?:\+(?P<buildmetadata>[0-9a-zA-Z-]+(?:\.[0-9a-zA-Z-]+)*))?` //nolint:lll // cannot split regex in multiple lines.
