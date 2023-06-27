bbd8438cc35970ab28c679241250fee220d7314c
Claudio Beatrice <claudi0.beatric3@gmail.com>
2023-06-27 16:08:11 +0200

    chore: move semver regex to semver package

--- internal/x/bytes/transform.go
+++ internal/x/bytes/transform.go
@@ -23,7 +23,7 @@ type TransformFunc func([]byte) ([]byte, error)
 // Author: https://github.com/acarl005
 // License: MIT License.
 
-const ansi = "[\u001B\u009B][[\\]()#;?]*(?:(?:(?:[a-zA-Z\\d]*(?:;[a-zA-Z\\d]*)*)?\u0007)|(?:(?:\\d{1,4}(?:;\\d{0,4})*)?[\\dA-PRZcf-ntqry=><~]))" //nolint:lll // Cannot split regex in multiple lines.
+const ansi = "[\u001B\u009B][[\\]()#;?]*(?:(?:(?:[a-zA-Z\\d]*(?:;[a-zA-Z\\d]*)*)?\u0007)|(?:(?:\\d{1,4}(?:;\\d{0,4})*)?[\\dA-PRZcf-ntqry=><~]))" //nolint:lll // cannot split regex in multiple lines.
 
 var (
 	ErrJSONTransform = errors.New("error while transform to json")
