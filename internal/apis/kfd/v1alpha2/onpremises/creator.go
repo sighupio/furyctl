00c85df15f85fb1872d5b89c68f78b15b686da2d
Alessio Dionisi <me@alessiodionisi.com>
2023-08-03 08:33:56 +0200

    feat: init onpremises support

---
+++ internal/apis/kfd/v1alpha2/onpremises/creator.go
@@ -0,0 +1,80 @@
+// Copyright (c) 2017-present SIGHUP s.r.l All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package onpremises
+
+import (
+	"strings"
+
+	"github.com/sighupio/fury-distribution/pkg/apis/config"
+	"github.com/sighupio/fury-distribution/pkg/apis/onpremises/v1alpha2/public"
+	"github.com/sighupio/furyctl/internal/cluster"
+)
+
+type ClusterCreator struct {
+	paths       cluster.CreatorPaths
+	furyctlConf public.OnpremisesKfdV1Alpha2
+	kfdManifest config.KFD
+	phase       string
+	dryRun      bool
+}
+
+func (c *ClusterCreator) SetProperties(props []cluster.CreatorProperty) {
+	for _, prop := range props {
+		c.SetProperty(prop.Name, prop.Value)
+	}
+}
+
+func (c *ClusterCreator) SetProperty(name string, value any) {
+	switch strings.ToLower(name) {
+	case cluster.CreatorPropertyConfigPath:
+		if s, ok := value.(string); ok {
+			c.paths.ConfigPath = s
+		}
+
+	case cluster.CreatorPropertyDistroPath:
+		if s, ok := value.(string); ok {
+			c.paths.DistroPath = s
+		}
+
+	case cluster.CreatorPropertyWorkDir:
+		if s, ok := value.(string); ok {
+			c.paths.WorkDir = s
+		}
+
+	case cluster.CreatorPropertyBinPath:
+		if s, ok := value.(string); ok {
+			c.paths.BinPath = s
+		}
+
+	case cluster.CreatorPropertyKubeconfig:
+		if s, ok := value.(string); ok {
+			c.paths.Kubeconfig = s
+		}
+
+	case cluster.CreatorPropertyFuryctlConf:
+		if s, ok := value.(public.OnpremisesKfdV1Alpha2); ok {
+			c.furyctlConf = s
+		}
+
+	case cluster.CreatorPropertyKfdManifest:
+		if s, ok := value.(config.KFD); ok {
+			c.kfdManifest = s
+		}
+
+	case cluster.CreatorPropertyPhase:
+		if s, ok := value.(string); ok {
+			c.phase = s
+		}
+
+	case cluster.CreatorPropertyDryRun:
+		if b, ok := value.(bool); ok {
+			c.dryRun = b
+		}
+	}
+}
+
+func (c *ClusterCreator) Create(_ string, _ int) error {
+	return nil
+}
