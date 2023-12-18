b2c5ec53ca726c5206cdeb82c1fa5369d51c8eba
Alessio Dionisi <me@alessiodionisi.com>
2023-12-18 15:16:34 +0100

    feat: parallel tests

--- test/expensive/ekscluster/furyctl_test.go
+++ test/expensive/ekscluster/furyctl_test.go
@@ -37,90 +37,94 @@ var (
 
 	_ = BeforeSuite(CompileFuryctl(furyctl))
 
-	BeforeCreateDeleteTestFunc = func(state *ContextState, version, furyctlYamlTemplate string) func() {
-		return func() {
-			*state = NewContextState(fmt.Sprintf("ekscluster-v%s-create-and-delete", version))
+	PrepareCreateDeleteClusterTest = func(state *ContextState, version, furyctlYamlTemplate string) {
+		*state = NewContextState(fmt.Sprintf("ekscluster-v%s-create-and-delete", version))
 
-			GinkgoWriter.Write([]byte(fmt.Sprintf("Test id: %d", state.TestID)))
+		state.Kubeconfig = path.Join(
+			state.TestDir,
+			state.ClusterName,
+			cluster.OperationPhaseKubernetes,
+			"terraform",
+			"secrets",
+			"kubeconfig",
+		)
 
-			Copy("./testdata/id_ed25519", path.Join(state.TestDir, "id_ed25519"))
-			Copy("./testdata/id_ed25519.pub", path.Join(state.TestDir, "id_ed25519.pub"))
+		GinkgoWriter.Write([]byte(fmt.Sprintf("Test id: %d", state.TestID)))
 
-			CreateFuryctlYaml(state, furyctlYamlTemplate, nil)
-		}
+		Copy("./testdata/id_ed25519", path.Join(state.TestDir, "id_ed25519"))
+		Copy("./testdata/id_ed25519.pub", path.Join(state.TestDir, "id_ed25519.pub"))
+
+		CreateFuryctlYaml(state, furyctlYamlTemplate, nil)
 	}
 
-	CreateClusterTestFunc = func(state *ContextState) func() {
-		return func() {
-			dlRes := DownloadFuryDistribution(state.FuryctlYaml)
+	CreateClusterTest = func(state *ContextState) {
+		dlRes := DownloadFuryDistribution(state.FuryctlYaml)
 
-			tfPlanPath := path.Join(
-				state.HomeDir,
-				".furyctl",
-				state.ClusterName,
-				cluster.OperationPhaseInfrastructure,
-				"terraform",
-				"plan",
-				"terraform.plan",
-			)
+		tfPlanPath := path.Join(
+			state.TestDir,
+			state.ClusterName,
+			cluster.OperationPhaseInfrastructure,
+			"terraform",
+			"plan",
+			"terraform.plan",
+		)
 
-			furyctlCreator := NewFuryctlCreator(
-				furyctl,
-				state.FuryctlYaml,
-				state.TmpDir,
-				false,
-			)
+		furyctlCreator := NewFuryctlCreator(
+			furyctl,
+			state.FuryctlYaml,
+			state.TmpDir,
+			state.TestDir,
+			false,
+		)
 
-			createClusterCmd := furyctlCreator.Create(
-				cluster.OperationPhaseAll,
-				"",
-			)
+		createClusterCmd := furyctlCreator.Create(
+			cluster.OperationPhaseAll,
+			"",
+		)
 
-			session := Must1(gexec.Start(createClusterCmd, GinkgoWriter, GinkgoWriter))
+		session := Must1(gexec.Start(createClusterCmd, GinkgoWriter, GinkgoWriter))
 
-			Consistently(session, 3*time.Minute).ShouldNot(gexec.Exit())
+		Consistently(session, 3*time.Minute).ShouldNot(gexec.Exit())
 
-			Eventually(tfPlanPath, assertTimeout, assertPollingInterval).Should(BeAnExistingFile())
-			Eventually(state.Kubeconfig, assertTimeout, assertPollingInterval).Should(BeAnExistingFile())
-			Eventually(session, assertTimeout, assertPollingInterval).Should(gexec.Exit(0))
+		Eventually(tfPlanPath, assertTimeout, assertPollingInterval).Should(BeAnExistingFile())
+		Eventually(state.Kubeconfig, assertTimeout, assertPollingInterval).Should(BeAnExistingFile())
+		Eventually(session, assertTimeout, assertPollingInterval).Should(gexec.Exit(0))
 
-			kubectlPath := DownloadKubectl(dlRes.DistroManifest.Tools.Common.Kubectl.Version)
-			kubeCmd := exec.Command(kubectlPath, "--kubeconfig", state.Kubeconfig, "get", "nodes")
+		kubectlPath := DownloadKubectl(dlRes.DistroManifest.Tools.Common.Kubectl.Version)
+		kubeCmd := exec.Command(kubectlPath, "--kubeconfig", state.Kubeconfig, "get", "nodes")
 
-			kubeSession, err := gexec.Start(kubeCmd, GinkgoWriter, GinkgoWriter)
+		kubeSession, err := gexec.Start(kubeCmd, GinkgoWriter, GinkgoWriter)
 
-			Expect(err).To(Not(HaveOccurred()))
-			Eventually(kubeSession, assertTimeout, assertPollingInterval).Should(gexec.Exit(0))
-		}
+		Expect(err).To(Not(HaveOccurred()))
+		Eventually(kubeSession, assertTimeout, assertPollingInterval).Should(gexec.Exit(0))
 	}
 
-	DeleteClusterTestFunc = func(state *ContextState) func() {
-		return func() {
-			DeferCleanup(func() {
-				_ = os.RemoveAll(state.TmpDir)
+	DeleteClusterTest = func(state *ContextState) {
+		DeferCleanup(func() {
+			_ = os.RemoveAll(state.TmpDir)
 
-				pkillSession := Must1(KillOpenVPN())
+			pkillSession := Must1(KillOpenVPN())
 
-				Eventually(pkillSession, 5*time.Minute, 1*time.Second).Should(gexec.Exit(0))
-			})
+			Eventually(pkillSession, 5*time.Minute, 1*time.Second).Should(gexec.Exit(0))
+		})
 
-			furyctlDeleter := NewFuryctlDeleter(
-				furyctl,
-				state.FuryctlYaml,
-				state.DistroDir,
-				state.TmpDir,
-				false,
-			)
+		furyctlDeleter := NewFuryctlDeleter(
+			furyctl,
+			state.FuryctlYaml,
+			state.DistroDir,
+			state.TmpDir,
+			state.TestDir,
+			false,
+		)
 
-			deleteClusterCmd := furyctlDeleter.Delete(
-				cluster.OperationPhaseAll,
-			)
+		deleteClusterCmd := furyctlDeleter.Delete(
+			cluster.OperationPhaseAll,
+		)
 
-			session, err := gexec.Start(deleteClusterCmd, GinkgoWriter, GinkgoWriter)
+		session, err := gexec.Start(deleteClusterCmd, GinkgoWriter, GinkgoWriter)
 
-			Expect(err).To(Not(HaveOccurred()))
-			Eventually(session, assertTimeout, assertPollingInterval).Should(gexec.Exit(0))
-		}
+		Expect(err).To(Not(HaveOccurred()))
+		Eventually(session, assertTimeout, assertPollingInterval).Should(gexec.Exit(0))
 	}
 
 	CreateAndDeleteTestScenario = func(version string) func() {
@@ -135,12 +139,15 @@ var (
 
 			contextTitle := fmt.Sprintf("v%s create and delete a minimal public cluster", version)
 
-			Context(contextTitle, Ordered, Serial, Label("slow"), func() {
-				BeforeAll(BeforeCreateDeleteTestFunc(state, version, "furyctl-public-minimal.yaml.tpl"))
+			Context(contextTitle, Label("slow"), func() {
+				It(fmt.Sprintf("should create and delete a minimal public %s cluster", version), func(s *ContextState) func() {
+					return func() {
+						PrepareCreateDeleteClusterTest(state, version, "furyctl-public-minimal.yaml.tpl")
 
-				It(fmt.Sprintf("should create a minimal public %s cluster", version), Serial, CreateClusterTestFunc(state))
-
-				It(fmt.Sprintf("should delete a minimal public %s cluster", version), Serial, DeleteClusterTestFunc(state))
+						CreateClusterTest(s)
+						DeleteClusterTest(s)
+					}
+				}(state))
 			})
 		}
 	}
@@ -153,12 +160,12 @@ var (
 	// _ = Describe("furyctl & distro v1.25.5 - public minimal", Ordered, Serial, CreateAndDeleteTestScenario("1.25.5"))
 	// _ = Describe("furyctl & distro v1.25.6 - public minimal", Ordered, Serial, CreateAndDeleteTestScenario("1.25.6"))
 	// _ = Describe("furyctl & distro v1.25.7 - public minimal", Ordered, Serial, CreateAndDeleteTestScenario("1.25.7"))
-	_ = Describe("furyctl & distro v1.25.8 - public minimal", Ordered, Serial, CreateAndDeleteTestScenario("1.25.8"))
+	_ = Describe("furyctl & distro v1.25.8 - public minimal", CreateAndDeleteTestScenario("1.25.8"))
 
 	// _ = Describe("furyctl & distro v1.26.0 - public minimal", Ordered, Serial, CreateAndDeleteTestScenario("1.26.0"))
 	// _ = Describe("furyctl & distro v1.26.1 - public minimal", Ordered, Serial, CreateAndDeleteTestScenario("1.26.1"))
 	// _ = Describe("furyctl & distro v1.26.2 - public minimal", Ordered, Serial, CreateAndDeleteTestScenario("1.26.2"))
-	_ = Describe("furyctl & distro v1.26.3 - public minimal", Ordered, Serial, CreateAndDeleteTestScenario("1.26.3"))
+	_ = Describe("furyctl & distro v1.26.3 - public minimal", CreateAndDeleteTestScenario("1.26.3"))
 
 	// _ = Describe("furyctl & distro v1.27.0 - public minimal", Ordered, Serial, CreateAndDeleteTestScenario("1.27.0"))
 )
