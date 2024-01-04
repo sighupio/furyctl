2308b72ee0ea15a7885cc9ae59b7b53b119db7d2
Alessio Dionisi <me@alessiodionisi.com>
2024-01-04 15:03:21 +0100

    feat: remove old kubeconfig flag

--- cmd/errors.go
+++ cmd/errors.go
@@ -7,7 +7,5 @@ package cmd
 import "errors"
 
 var (
-	ErrParsingFlag        = errors.New("error while parsing flag")
-	ErrKubeconfigReq      = errors.New("either the KUBECONFIG environment variable or the --kubeconfig flag should be set")
-	ErrKubeconfigNotFound = errors.New("kubeconfig file not found")
+	ErrParsingFlag = errors.New("error while parsing flag")
 )
