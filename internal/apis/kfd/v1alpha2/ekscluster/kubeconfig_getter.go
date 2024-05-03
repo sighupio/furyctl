379b22cb2cd67bfbd3c3cdfe470abf954a2a3938
Alessio Pragliola <83355398+Al-Pragliola@users.noreply.github.com>
2024-05-03 14:15:43 +0200

    feat: added get kubeconfig sub command (#513)
    * feat: added get config sub command
    
    * chore: added success message

---
+++ internal/apis/kfd/v1alpha2/ekscluster/kubeconfig_getter.go
@@ -0,0 +1,80 @@
+// Copyright (c) 2017-present SIGHUP s.r.l All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package ekscluster
+
+import (
+	"fmt"
+	"path"
+	"strings"
+
+	"github.com/sirupsen/logrus"
+
+	"github.com/sighupio/fury-distribution/pkg/apis/ekscluster/v1alpha2/private"
+	"github.com/sighupio/furyctl/internal/cluster"
+	"github.com/sighupio/furyctl/internal/tool/awscli"
+	execx "github.com/sighupio/furyctl/internal/x/exec"
+)
+
+type KubeconfigGetter struct {
+	furyctlConf private.EksclusterKfdV1Alpha2
+	configPath  string
+	outDir      string
+}
+
+func (k *KubeconfigGetter) SetProperties(props []cluster.KubeconfigProperty) {
+	for _, prop := range props {
+		k.SetProperty(prop.Name, prop.Value)
+	}
+}
+
+func (k *KubeconfigGetter) SetProperty(name string, value any) {
+	lcName := strings.ToLower(name)
+
+	switch lcName {
+	case cluster.KubeconfigPropertyFuryctlConf:
+		if s, ok := value.(private.EksclusterKfdV1Alpha2); ok {
+			k.furyctlConf = s
+		}
+
+	case cluster.KubeconfigPropertyConfigPath:
+		if s, ok := value.(string); ok {
+			k.configPath = s
+		}
+
+	case cluster.KubeconfigPropertyOutdir:
+		if s, ok := value.(string); ok {
+			k.outDir = s
+		}
+	}
+}
+
+func (k *KubeconfigGetter) Get() error {
+	logrus.Info("Getting kubeconfig...")
+
+	kubeconfigPath := path.Join(k.outDir, "kubeconfig")
+
+	awsRunner := awscli.NewRunner(
+		execx.NewStdExecutor(),
+		awscli.Paths{
+			Awscli:  "aws",
+			WorkDir: k.outDir,
+		},
+	)
+
+	if _, err := awsRunner.Eks(
+		false,
+		"update-kubeconfig",
+		"--name",
+		k.furyctlConf.Metadata.Name,
+		"--kubeconfig",
+		kubeconfigPath,
+		"--region",
+		string(k.furyctlConf.Spec.Region),
+	); err != nil {
+		return fmt.Errorf("error getting kubeconfig: %w", err)
+	}
+
+	return nil
+}
--- internal/apis/kfd/v1alpha2/kfddistribution/init.go
+++ internal/apis/kfd/v1alpha2/kfddistribution/init.go
@@ -22,4 +22,10 @@ func init() {
 		"KFDDistribution",
 		cluster.NewDeleterFactory[*ClusterDeleter, public.KfddistributionKfdV1Alpha2](&ClusterDeleter{}),
 	)
+
+	cluster.RegisterKubeconfigFactory(
+		"kfd.sighup.io/v1alpha2",
+		"KFDDistribution",
+		cluster.NewKubeconfigFactory[*KubeconfigGetter, public.KfddistributionKfdV1Alpha2](&KubeconfigGetter{}),
+	)
 }
