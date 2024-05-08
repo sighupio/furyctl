e5ea3a40702f1d208bed454a4e5d762d27f3b397
Samuele Chiocca <samuele@sighup.io>
2024-05-08 16:28:18 +0200

    fix: missing feature gate on validator.go for kapp

--- internal/dependencies/tools/validator.go
+++ internal/dependencies/tools/validator.go
@@ -115,6 +115,10 @@ func (tv *Validator) validateTools(i any, kfdManifest config.KFD) ([]string, []e
 			continue
 		}
 
+		if (toolName == "kapp") && !distribution.HasFeature(kfdManifest, distribution.FeatureKappSupport) {
+			continue
+		}
+
 		tool := tv.toolFactory.Create(itool.Name(toolName), toolCfg.Version)
 		if err := tool.CheckBinVersion(); err != nil {
 			errs = append(errs, err)
