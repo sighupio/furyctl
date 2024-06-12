e25afdafb3982631ca7d57cf77778bc525ca2be1
Alessio Pragliola <alessio.pragliola@sighup.io>
2024-06-12 17:42:40 +0200

    fix: linting

--- internal/apis/kfd/v1alpha2/ekscluster/schema.go
+++ internal/apis/kfd/v1alpha2/ekscluster/schema.go
@@ -5,13 +5,18 @@
 package ekscluster
 
 import (
+	"errors"
 	"fmt"
 
 	"github.com/sighupio/fury-distribution/pkg/apis/ekscluster/v1alpha2/private"
 	yamlx "github.com/sighupio/furyctl/internal/x/yaml"
 )
 
-var ErrInvalidNodePoolSize = fmt.Errorf("invalid node pool size")
+var (
+	ErrInvalidNodePoolSize = errors.New("invalid node pool size")
+	ErrVPNBucketNamePrefix = errors.New("vpn bucketNamePrefix is required when cluster name is longer than 19 characters")         //nolint: lll // long line
+	ErrIamNameOverride     = errors.New("vpn iamResourcesNameOverride is required when cluster name is longer than 19 characters") //nolint: lll // long line
+)
 
 type ExtraSchemaValidator struct{}
 
@@ -30,6 +35,25 @@ func (*ExtraSchemaValidator) Validate(confPath string) error {
 				nodePool.Size.Max,
 				nodePool.Size.Min,
 			)
+		}
+	}
+
+	//nolint:revive // ignore magic number linters
+	if len(furyctlConf.Metadata.Name) > 19 && furyctlConf.Spec.Infrastructure != nil &&
+		furyctlConf.Spec.Infrastructure.Vpn != nil &&
+		(furyctlConf.Spec.Infrastructure.Vpn.Instances == nil ||
+			(furyctlConf.Spec.Infrastructure.Vpn.Instances != nil &&
+				*furyctlConf.Spec.Infrastructure.Vpn.Instances > 0)) {
+		if furyctlConf.Spec.Infrastructure.Vpn.IamResourcesNameOverride == nil ||
+			(furyctlConf.Spec.Infrastructure.Vpn.IamResourcesNameOverride != nil &&
+				*furyctlConf.Spec.Infrastructure.Vpn.IamResourcesNameOverride == "") {
+			return ErrIamNameOverride
+		}
+
+		if furyctlConf.Spec.Infrastructure.Vpn.BucketNamePrefix == nil ||
+			(furyctlConf.Spec.Infrastructure.Vpn.BucketNamePrefix != nil &&
+				*furyctlConf.Spec.Infrastructure.Vpn.BucketNamePrefix == "") {
+			return ErrVPNBucketNamePrefix
 		}
 	}
 
