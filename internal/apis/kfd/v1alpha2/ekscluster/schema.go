a39d1b9c7f1a379f0c75f013886b1fe5b78939de
Alessio Pragliola <alessio.pragliola@sighup.io>
2024-06-14 17:11:00 +0200

    feat: added extra schema validation for cluster names longer than 19 chars

--- internal/apis/kfd/v1alpha2/ekscluster/schema.go
+++ internal/apis/kfd/v1alpha2/ekscluster/schema.go
@@ -13,14 +13,19 @@ import (
 )
 
 var (
-	ErrInvalidNodePoolSize = errors.New("invalid node pool size")
-	ErrVPNBucketNamePrefix = errors.New("vpn bucketNamePrefix is required when cluster name is longer than 19 characters")         //nolint: lll // long line
-	ErrIamNameOverride     = errors.New("vpn iamResourcesNameOverride is required when cluster name is longer than 19 characters") //nolint: lll // long line
+	ErrInvalidNodePoolSize                  = errors.New("invalid node pool size")
+	ErrVPNBucketNamePrefix                  = errors.New(".spec.infrastructure.vpn.bucketNamePrefix is required when cluster name is longer than 19 characters")                                   //nolint: lll // long line
+	ErrIAMUserNameOverride                  = errors.New(".spec.infrastructure.vpn.iamUserNameOverride is required when cluster name is longer than 19 characters")                                //nolint: lll // long line
+	ErrClusterIAMRoleNamePrefixOverride     = errors.New(".spec.kubernetes.clusterIAMRoleNamePrefixOverride is required when cluster name is longer than 40 characters")                           //nolint: lll // long line
+	ErrWorkersIAMRoleNamePrefixOverride     = errors.New(".spec.kubernetes.workersIAMRoleNamePrefixOverride is required when cluster name is longer than 40 characters")                           //nolint: lll // long line
+	ErrEBSCSIDriverIAMRoleNameOverride      = errors.New(".spec.distribution.modules.aws.ebsCsiDriver.overrides.iamRoleName is required when cluster name is longer than 40 characters")           //nolint: lll // long line
+	ErrClusterAutoscalerIAMRoleNameOverride = errors.New(".spec.distribution.modules.aws.clusterAutoscaler.overrides.iamRoleName is required when cluster name is longer than 40 characters")      //nolint: lll // long line
+	ErrLBControllerIAMRoleNameOverride      = errors.New(".spec.distribution.modules.aws.loadbalancerController.overrides.iamRoleName is required when cluster name is longer than 40 characters") //nolint: lll // long line
 )
 
 type ExtraSchemaValidator struct{}
 
-func (*ExtraSchemaValidator) Validate(confPath string) error {
+func (v *ExtraSchemaValidator) Validate(confPath string) error {
 	furyctlConf, err := yamlx.FromFileV3[private.EksclusterKfdV1Alpha2](confPath)
 	if err != nil {
 		return err
@@ -38,16 +43,32 @@ func (*ExtraSchemaValidator) Validate(confPath string) error {
 		}
 	}
 
+	return v.validateClusterName(&furyctlConf)
+}
+
+func (v *ExtraSchemaValidator) validateClusterName(furyctlConf *private.EksclusterKfdV1Alpha2) error {
+	if err := v.validateInfraVPNOverrides(furyctlConf); err != nil {
+		return err
+	}
+
+	if err := v.validateKubernetesOverrides(furyctlConf); err != nil {
+		return err
+	}
+
+	return v.validateAWSModuleOverrides(furyctlConf)
+}
+
+func (*ExtraSchemaValidator) validateInfraVPNOverrides(furyctlConf *private.EksclusterKfdV1Alpha2) error {
 	//nolint:revive // ignore magic number linters
 	if len(furyctlConf.Metadata.Name) > 19 && furyctlConf.Spec.Infrastructure != nil &&
 		furyctlConf.Spec.Infrastructure.Vpn != nil &&
 		(furyctlConf.Spec.Infrastructure.Vpn.Instances == nil ||
 			(furyctlConf.Spec.Infrastructure.Vpn.Instances != nil &&
 				*furyctlConf.Spec.Infrastructure.Vpn.Instances > 0)) {
-		if furyctlConf.Spec.Infrastructure.Vpn.IamResourcesNameOverride == nil ||
-			(furyctlConf.Spec.Infrastructure.Vpn.IamResourcesNameOverride != nil &&
-				*furyctlConf.Spec.Infrastructure.Vpn.IamResourcesNameOverride == "") {
-			return ErrIamNameOverride
+		if furyctlConf.Spec.Infrastructure.Vpn.IamUserNameOverride == nil ||
+			(furyctlConf.Spec.Infrastructure.Vpn.IamUserNameOverride != nil &&
+				*furyctlConf.Spec.Infrastructure.Vpn.IamUserNameOverride == "") {
+			return ErrIAMUserNameOverride
 		}
 
 		if furyctlConf.Spec.Infrastructure.Vpn.BucketNamePrefix == nil ||
@@ -55,6 +76,98 @@ func (*ExtraSchemaValidator) Validate(confPath string) error {
 				*furyctlConf.Spec.Infrastructure.Vpn.BucketNamePrefix == "") {
 			return ErrVPNBucketNamePrefix
 		}
+	}
+
+	return nil
+}
+
+func (*ExtraSchemaValidator) validateKubernetesOverrides(furyctlConf *private.EksclusterKfdV1Alpha2) error {
+	//nolint:revive,gomnd,mnd // ignore magic number linters
+	if len(furyctlConf.Metadata.Name) <= 40 {
+		return nil
+	}
+
+	if furyctlConf.Spec.Kubernetes.ClusterIAMRoleNamePrefixOverride == nil ||
+		(furyctlConf.Spec.Kubernetes.ClusterIAMRoleNamePrefixOverride != nil &&
+			*furyctlConf.Spec.Kubernetes.ClusterIAMRoleNamePrefixOverride == "") {
+		return ErrClusterIAMRoleNamePrefixOverride
+	}
+
+	if furyctlConf.Spec.Kubernetes.WorkersIAMRoleNamePrefixOverride == nil ||
+		(furyctlConf.Spec.Kubernetes.WorkersIAMRoleNamePrefixOverride != nil &&
+			*furyctlConf.Spec.Kubernetes.WorkersIAMRoleNamePrefixOverride == "") {
+		return ErrWorkersIAMRoleNamePrefixOverride
+	}
+
+	return nil
+}
+
+func (v *ExtraSchemaValidator) validateAWSModuleOverrides(furyctlConf *private.EksclusterKfdV1Alpha2) error {
+	//nolint:revive,gomnd,mnd // ignore magic number linters
+	if len(furyctlConf.Metadata.Name) <= 40 {
+		return nil
+	}
+
+	if err := v.validateEbsCsiDriverOverride(furyctlConf); err != nil {
+		return err
+	}
+
+	if err := v.validateClusterAutoscalerOverride(furyctlConf); err != nil {
+		return err
+	}
+
+	return v.validateLoadBalancerControllerOverride(furyctlConf)
+}
+
+//nolint:dupl // false positive
+func (*ExtraSchemaValidator) validateEbsCsiDriverOverride(furyctlConf *private.EksclusterKfdV1Alpha2) error {
+	if furyctlConf.Spec.Distribution.Modules.Aws == nil ||
+		(furyctlConf.Spec.Distribution.Modules.Aws != nil &&
+			furyctlConf.Spec.Distribution.Modules.Aws.EbsCsiDriver.Overrides == nil) ||
+		(furyctlConf.Spec.Distribution.Modules.Aws != nil &&
+			furyctlConf.Spec.Distribution.Modules.Aws.EbsCsiDriver.Overrides != nil &&
+			furyctlConf.Spec.Distribution.Modules.Aws.EbsCsiDriver.Overrides.IamRoleName == nil) ||
+		(furyctlConf.Spec.Distribution.Modules.Aws != nil &&
+			furyctlConf.Spec.Distribution.Modules.Aws.EbsCsiDriver.Overrides != nil &&
+			furyctlConf.Spec.Distribution.Modules.Aws.EbsCsiDriver.Overrides.IamRoleName != nil &&
+			*furyctlConf.Spec.Distribution.Modules.Aws.EbsCsiDriver.Overrides.IamRoleName == "") {
+		return ErrEBSCSIDriverIAMRoleNameOverride
+	}
+
+	return nil
+}
+
+//nolint:dupl // false positive
+func (*ExtraSchemaValidator) validateClusterAutoscalerOverride(furyctlConf *private.EksclusterKfdV1Alpha2) error {
+	if furyctlConf.Spec.Distribution.Modules.Aws == nil ||
+		(furyctlConf.Spec.Distribution.Modules.Aws != nil &&
+			furyctlConf.Spec.Distribution.Modules.Aws.ClusterAutoscaler.Overrides == nil) ||
+		(furyctlConf.Spec.Distribution.Modules.Aws != nil &&
+			furyctlConf.Spec.Distribution.Modules.Aws.ClusterAutoscaler.Overrides != nil &&
+			furyctlConf.Spec.Distribution.Modules.Aws.ClusterAutoscaler.Overrides.IamRoleName == nil) ||
+		(furyctlConf.Spec.Distribution.Modules.Aws != nil &&
+			furyctlConf.Spec.Distribution.Modules.Aws.ClusterAutoscaler.Overrides != nil &&
+			furyctlConf.Spec.Distribution.Modules.Aws.ClusterAutoscaler.Overrides.IamRoleName != nil &&
+			*furyctlConf.Spec.Distribution.Modules.Aws.ClusterAutoscaler.Overrides.IamRoleName == "") {
+		return ErrClusterAutoscalerIAMRoleNameOverride
+	}
+
+	return nil
+}
+
+//nolint:dupl // false positive
+func (*ExtraSchemaValidator) validateLoadBalancerControllerOverride(furyctlConf *private.EksclusterKfdV1Alpha2) error {
+	if furyctlConf.Spec.Distribution.Modules.Aws == nil ||
+		(furyctlConf.Spec.Distribution.Modules.Aws != nil &&
+			furyctlConf.Spec.Distribution.Modules.Aws.LoadBalancerController.Overrides == nil) ||
+		(furyctlConf.Spec.Distribution.Modules.Aws != nil &&
+			furyctlConf.Spec.Distribution.Modules.Aws.LoadBalancerController.Overrides != nil &&
+			furyctlConf.Spec.Distribution.Modules.Aws.LoadBalancerController.Overrides.IamRoleName == nil) ||
+		(furyctlConf.Spec.Distribution.Modules.Aws != nil &&
+			furyctlConf.Spec.Distribution.Modules.Aws.LoadBalancerController.Overrides != nil &&
+			furyctlConf.Spec.Distribution.Modules.Aws.LoadBalancerController.Overrides.IamRoleName != nil &&
+			*furyctlConf.Spec.Distribution.Modules.Aws.LoadBalancerController.Overrides.IamRoleName == "") {
+		return ErrLBControllerIAMRoleNameOverride
 	}
 
 	return nil
