aeac9b6fd61737ba0e1134f210659f7b17954372
Ramiro Algozino <ramiro@sighup.io>
2025-03-20 12:38:29 +0100

    chore: improve unsupported version message
    Mention the new get supported-versions command

--- pkg/distribution/download.go
+++ pkg/distribution/download.go
@@ -94,7 +94,8 @@ func (d *Downloader) Download(
 
 	if !compatChecker.IsCompatible() {
 		logrus.Warnf("The specified KFD version %s is not supported by this version of furyctl, "+
-			"please upgrade furyctl to the latest version or use a supported KFD version",
+			"please upgrade furyctl to the latest version or use a supported KFD version. "+
+			"Run `furyctl get supported-versions` for a compatibility matrix.",
 			minimalConf.Spec.DistributionVersion)
 	}
 
