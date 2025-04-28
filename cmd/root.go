be40d84bdea42245366b7ce9b147d2f88118f435
Ramiro Algozino <ramiro@sighup.io>
2025-04-28 16:17:04 +0200

    chore: help message formatting

--- cmd/root.go
+++ cmd/root.go
@@ -52,8 +52,7 @@ func NewRootCmd() *RootCommand {
 			Short: "The Swiss Army knife for the SIGHUP Distribution",
 			Long: `The multi-purpose command line tool for the SIGHUP Distribution.
 
-	furyctl is a command line interface tool to manage the full lifecycle of a SIGHUP Distribution Cluster.
-	`,
+furyctl is a command line interface tool to manage the full lifecycle of SIGHUP Distribution Clusters.`,
 			SilenceUsage:  true,
 			SilenceErrors: true,
 			PersistentPreRun: func(cmd *cobra.Command, _ []string) {
