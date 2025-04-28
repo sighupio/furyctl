d741b66f031441d6e0a9e0d26420b72a0ab3541e
Stefano Ghinelli <stefano.ghinelli@sighup.io>
2025-04-28 17:57:37 +0200

    chore: run make gci

--- cmd/get/supported-versions.go
+++ cmd/get/supported-versions.go
@@ -22,92 +22,92 @@ import (
 const DateFmt = "2006-01-02"
 
 func NewSupportedVersionsCmd() *cobra.Command {
-    var cmdEvent analytics.Event
+	var cmdEvent analytics.Event
 
-    kinds := []string{distribution.EKSClusterKind, distribution.KFDDistributionKind, distribution.OnPremisesKind}
-    
-    supportedVersionCmd := &cobra.Command{
-        Use:   "supported-versions",
-        Short: "List of currently supported SD versions and their compatibility with this version of furyctl for each kind.",
-        Long: `List of currently supported SD versions and their compatibility with this version of furyctl for each kind. If the "--kind" parameter is specified, the command will only provide information about the selected provider.
+	kinds := []string{distribution.EKSClusterKind, distribution.KFDDistributionKind, distribution.OnPremisesKind}
+
+	supportedVersionCmd := &cobra.Command{
+		Use:   "supported-versions",
+		Short: "List of currently supported SD versions and their compatibility with this version of furyctl for each kind.",
+		Long: `List of currently supported SD versions and their compatibility with this version of furyctl for each kind. If the "--kind" parameter is specified, the command will only provide information about the selected provider.
         Examples:
  - furyctl get supported-versions                  	will list the currently supported SD versions and their compatibility with this version of furyctl for each kind.
  - furyctl get supported-versions --kind OnPremises	will list the currently supported SD versions and their compatibility with this version of furyctl but for the OnPremises kind.
         `,
-        PreRun: func(cmd *cobra.Command, _ []string) {
-            cmdEvent = analytics.NewCommandEvent(cobrax.GetFullname(cmd))
-            if err := viper.BindPFlags(cmd.Flags()); err != nil {
-                logrus.Fatalf("error while binding flags: %v", err)
-            }
-        },
-        RunE: func(_ *cobra.Command, _ []string) error {
-            ctn := app.GetContainerInstance()
-            tracker := ctn.Tracker()
-            tracker.Flush()
+		PreRun: func(cmd *cobra.Command, _ []string) {
+			cmdEvent = analytics.NewCommandEvent(cobrax.GetFullname(cmd))
+			if err := viper.BindPFlags(cmd.Flags()); err != nil {
+				logrus.Fatalf("error while binding flags: %v", err)
+			}
+		},
+		RunE: func(_ *cobra.Command, _ []string) error {
+			ctn := app.GetContainerInstance()
+			tracker := ctn.Tracker()
+			tracker.Flush()
 
-            releases, err := distribution.GetSupportedVersions(git.NewGitHubClient())
-            if err != nil {
-                cmdEvent.AddErrorMessage(err)
-                tracker.Track(cmdEvent)
+			releases, err := distribution.GetSupportedVersions(git.NewGitHubClient())
+			if err != nil {
+				cmdEvent.AddErrorMessage(err)
+				tracker.Track(cmdEvent)
 
-                return fmt.Errorf("error getting supported SD versions: %w", err)
-            }
-            
-            kind := viper.GetString("kind")
-            validKind, err := validateKind(kind, kinds)
-            if err != nil {
-                cmdEvent.AddErrorMessage(err)
-                tracker.Track(cmdEvent)
+				return fmt.Errorf("error getting supported SD versions: %w", err)
+			}
 
-                return err
-            }
-            
-            kindsToPrint := kinds
-            msg := "list of currently supported SD versions and their compatibility with this version of furyctl for "
-            
-            if validKind != "" {
-                kindsToPrint = []string{validKind}
-                msg += validKind + "\n"
-            } else {
-                msg += "each kind\n"
-            }
-            
-            logrus.Info(msg + FormatSupportedVersions(releases, kindsToPrint))
-            cmdEvent.AddSuccessMessage("supported SD versions")
-            tracker.Track(cmdEvent)
-			
-            return nil
-        },
-    }
-    
-    supportedVersionCmd.Flags().StringP(
-        "kind",
-        "k",
-        "",
-        fmt.Sprintf("Show supported SD versions for the kind of cluster specified. Valid values: %s",
-            strings.Join(kinds, ", ")),
-    )
-    
-    return supportedVersionCmd
+			kind := viper.GetString("kind")
+			validKind, err := validateKind(kind, kinds)
+			if err != nil {
+				cmdEvent.AddErrorMessage(err)
+				tracker.Track(cmdEvent)
+
+				return err
+			}
+
+			kindsToPrint := kinds
+			msg := "list of currently supported SD versions and their compatibility with this version of furyctl for "
+
+			if validKind != "" {
+				kindsToPrint = []string{validKind}
+				msg += validKind + "\n"
+			} else {
+				msg += "each kind\n"
+			}
+
+			logrus.Info(msg + FormatSupportedVersions(releases, kindsToPrint))
+			cmdEvent.AddSuccessMessage("supported SD versions")
+			tracker.Track(cmdEvent)
+
+			return nil
+		},
+	}
+
+	supportedVersionCmd.Flags().StringP(
+		"kind",
+		"k",
+		"",
+		fmt.Sprintf("Show supported SD versions for the kind of cluster specified. Valid values: %s",
+			strings.Join(kinds, ", ")),
+	)
+
+	return supportedVersionCmd
 }
 
 func validateKind(kind string, validKinds []string) (string, error) {
-    if kind == "" {
-        return "", nil
-    }
-    
-    kindMap := make(map[string]string)
-    for _, k := range validKinds {
-        kindMap[strings.ToLower(k)] = k
-    }
-    
-    matchedKind, ok := kindMap[strings.ToLower(kind)]
-    if !ok {
-        return "", fmt.Errorf("invalid kind: %s. Valid values are: %s",
-            kind, strings.Join(validKinds, ", "))
-    }
-    
-    return matchedKind, nil
+	if kind == "" {
+		return "", nil
+	}
+
+	kindMap := make(map[string]string)
+	for _, k := range validKinds {
+		kindMap[strings.ToLower(k)] = k
+	}
+
+	matchedKind, ok := kindMap[strings.ToLower(kind)]
+	if !ok {
+		return "", fmt.Errorf("invalid kind: %s. Valid values are: %s",
+			kind, strings.Join(validKinds, ", "))
+	}
+
+	return matchedKind, nil
 }
 
 func FormatSupportedVersions(releases []distribution.KFDRelease, kinds []string) string {
