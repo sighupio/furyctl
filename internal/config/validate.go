98ec7c8dc4302d7ed4740af93e71fa8f7adcc4a9
Filo01 <git@filippo.sh>
2025-10-31 10:44:48 +0100

    fix: lint

--- internal/config/validate.go
+++ internal/config/validate.go
@@ -400,7 +400,7 @@ func validateToolsConfiguration(repoPath string, furyctlConf map[string]any) err
 
 	toolsConfig, exists := spec["toolsConfiguration"].(map[string]any)
 	if !exists {
-		// no error if there is no toolsConfiguration(e.g. not available in KFDDistribution)
+		// No error if there is no toolsConfiguration(e.g. not available in KFDDistribution)
 		return nil
 	}
 
