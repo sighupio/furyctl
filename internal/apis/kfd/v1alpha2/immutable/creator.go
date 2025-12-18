2000782e2855054bf65f9a48fca9833907cf8d20
Santiago Merlos <santiago.merlos@reevo.eu>
2025-12-18 13:51:48 +0100

    feat: add support for Immutable kind in KFD API
    - Introduced new Immutable kind in the KFD API with associated functionalities.
    - Implemented ClusterCreator, ClusterDeleter, CertificatesRenewer, and KubeconfigGetter for the Immutable kind.
    - Added ExtraSchemaValidator for Immutable kind to validate schemas.
    - Updated schema.go to include the new Immutable kind in the ExtraSchemaValidatorFactory.

---
+++ internal/apis/kfd/v1alpha2/immutable/creator.go
@@ -0,0 +1,68 @@
+// Copyright (c) 2017-present SIGHUP s.r.l All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package immutable
+
+import (
+	"fmt"
+	"strings"
+
+	"github.com/sighupio/fury-distribution/pkg/apis/config"
+	"github.com/sighupio/fury-distribution/pkg/apis/immutable/v1alpha2/public"
+	"github.com/sighupio/furyctl/internal/cluster"
+)
+
+type ClusterCreator struct {
+	paths       cluster.CreatorPaths
+	furyctlConf public.ImmutableKfdV1Alpha2
+	kfdManifest config.KFD
+}
+
+func (c *ClusterCreator) SetProperties(props []cluster.CreatorProperty) {
+	for _, prop := range props {
+		c.SetProperty(prop.Name, prop.Value)
+	}
+}
+
+func (c *ClusterCreator) SetProperty(name string, value any) {
+	switch strings.ToLower(name) {
+	case cluster.CreatorPropertyConfigPath:
+		if s, ok := value.(string); ok {
+			c.paths.ConfigPath = s
+		}
+
+	case cluster.CreatorPropertyDistroPath:
+		if s, ok := value.(string); ok {
+			c.paths.DistroPath = s
+		}
+
+	case cluster.CreatorPropertyWorkDir:
+		if s, ok := value.(string); ok {
+			c.paths.WorkDir = s
+		}
+
+	case cluster.CreatorPropertyBinPath:
+		if s, ok := value.(string); ok {
+			c.paths.BinPath = s
+		}
+
+	case cluster.CreatorPropertyFuryctlConf:
+		if s, ok := value.(public.ImmutableKfdV1Alpha2); ok {
+			c.furyctlConf = s
+		}
+
+	case cluster.CreatorPropertyKfdManifest:
+		if s, ok := value.(config.KFD); ok {
+			c.kfdManifest = s
+		}
+	}
+}
+
+func (c *ClusterCreator) Create(startFrom string, timeout, podRunningTimeout int) error {
+	return fmt.Errorf("cluster creation not implemented for Immutable kind")
+}
+
+func (c *ClusterCreator) GetPhasePath(phase string) (string, error) {
+	return "", fmt.Errorf("GetPhasePath not implemented for Immutable kind")
+}
